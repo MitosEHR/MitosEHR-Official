91d1df4ae30fc046c0936bf60e7d53d8d49a8dcf
Gino Rivera <hrivera@ideasgroupinc.com>
2011-01-30 15:52:22 -0400

    new file:   interface/calendar/calendar.ejs.php 	new file:   interface/calendar/load_combobox.ejs.php 	new file:   interface/calendar/load_events.ejs.php 	new file:   interface/calendar/load_form.ejs.php 	new file:   interface/calendar/manage_event.ejs.php 	new file:   interface/calendar/save_event.ejs.php 	new file:   interface/messages/component_data.ejs.php 	new file:   interface/messages/data_create.ejs.php 	new file:   interface/messages/data_destroy.ejs.php 	new file:   interface/messages/data_read.ejs.php 	new file:   interface/messages/data_update.ejs.php 	new file:   interface/messages/lab_results_messages.php 	new file:   interface/messages/messages.ejs.php 	new file:   ui_app/style_newui.css 	new file:   ui_icons/icoCalendar.png

---
+++ interface/messages/data_destroy.ejs.php
@@ -0,0 +1,64 @@
+<?php
+//--------------------------------------------------------------------------------------------------------------------------
+// manage_messages.ejs.php
+// v0.0.1
+// Under GPLv3 License
+//
+// Integration Sencha ExtJS Framework
+//
+// Integrated by: IdeasGroup Inc. in 2010
+//
+// OpenEMR is a free medical practice management, electronic medical records, prescription writing,
+// and medical billing application. These programs are also referred to as electronic health records.
+// OpenEMR is licensed under the General Gnu Public License (General GPL). It is a free open source replacement
+// for medical applications such as Medical Manager, Health Pro, and Misys. It features support for EDI billing
+// to clearing houses such as Availity, MD-Online, MedAvant and ZirMED using ANSI X12.
+//
+// Sencha ExtJS
+// Ext JS is a cross-browser JavaScript library for building rich internet applications. Build rich,
+// sustainable web applications faster than ever. It includes:
+// * High performance, customizable UI widgets
+// * Well designed and extensible Component model
+// * An intuitive, easy to use API
+// * Commercial and Open Source licenses available
+//
+// Remember, this file is called via the Framework Store, this is the AJAX thing.
+//--------------------------------------------------------------------------------------------------------------------------
+
+// *************************************************************************************
+//SANITIZE ALL ESCAPES
+// *************************************************************************************
+$sanitize_all_escapes=true;
+
+// *************************************************************************************
+//STOP FAKE REGISTER GLOBALS
+// *************************************************************************************
+$fake_register_globals=false;
+
+// *************************************************************************************
+// Load the OpenEMR Libraries
+// *************************************************************************************
+require_once("../../globals.php");
+require_once("$srcdir/pnotes.inc");
+require_once("$srcdir/patient.inc");
+require_once("$srcdir/acl.inc");
+require_once("$srcdir/log.inc");
+require_once("$srcdir/options.inc.php");
+require_once("$srcdir/formdata.inc.php");
+require_once("$srcdir/classes/Document.class.php");
+require_once("$srcdir/gprelations.inc.php");
+require_once("$srcdir/formatting.inc.php");
+
+// Count records variable
+$count = 0;
+
+// *************************************************************************************
+// Flag the message to delete
+// *************************************************************************************
+
+$data = json_decode ( $_POST['row'] );
+$delete_id = $data[0];
+deletePnote($delete_id);
+newEvent("delete", $_SESSION['authUser'], $_SESSION['authProvider'], 1, "pnotes: id ".$delete_id);
+
+?>
