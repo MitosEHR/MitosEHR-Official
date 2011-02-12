2477783ac894f5f82c1d9f3f0a223a528d3e8e2b
Gino Rivera <hrivera@mitosehr.org>
2011-02-12 19:00:02 -0400

    Fixed minor issued with Facilities screen, moving on.

---
+++ repository/dataExchange/dataExchange.inc.php
@@ -0,0 +1,25 @@
+<?php
+//*********************************************************************
+//
+// Functions to encode and decode data between Sencha and PHP
+//
+// Author: Gino Rivera Falú
+// Create Date: Feb 12 2011
+// Modified: 
+// Modified Date: 
+// Under LGPLv3 License
+//*********************************************************************
+
+
+// Encode the string to be safe for databases 
+function dataEncode($str){
+	return addslashes( trim($str) );
+}
+
+// Decode the string to be safe on JSON
+function dataDecode($str){
+	//$buff = htmlspecialchars_decode( trim($str), ENT_QUOTES );
+	return stripslashes($str);
+}
+
+?>
