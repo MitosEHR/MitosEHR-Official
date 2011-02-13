035757e9726808867aa008ae7be0a56e3541b29e
Gino Rivera <hrivera@mitosehr.org>
2011-02-12 21:54:03 -0400

    Upgrading to Sencha ExtJS 3.3.1

--- interface/registry.php
+++ interface/registry.php
@@ -379,8 +379,7 @@ if (version_compare(phpversion(), "5.2.1", ">=")) {
 ini_set("session.bug_compat_warn","off");
 
 // Sencha ExtJS Framework switcher
-$GLOBALS['ext_path'] = "ext-3.3.0";
-$GLOBALS['code_igniter_path'] = "CodeIgniter_2.0.0";
+$GLOBALS['ext_path'] = "ext-3.3.1";
 $GLOBALS['app_name'] = "MitosEHR";
 
 //////////////////////////////////////////////////////////////////
