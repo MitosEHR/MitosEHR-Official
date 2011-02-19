6f703e6d426eb41826f842e660a91b7d9ebe727d
Ernesto's iMac <erodriguez@TPRT001.local>
2011-02-19 04:39:58 -0400

    login using ext4 :: progress

--- interface/login/login.ejs4.php
+++ interface/login/login.ejs4.php
@@ -34,38 +34,85 @@ include_once("$srcdir/sql.inc.php");
  * http://www.sencha.com/license
  * 
  */
- Ext.require([
+Ext.require([
     'Ext.window.*'
 ]);
 Ext.onReady(function(){
 Ext.QuickTips.init();
 
+var winCopyright = new Ext.create('widget.window', {
+	id				: 'winCopyright',
+	title			: '<?php xl('MitosEHR Copyright Notice','e'); ?>',
+	closeAction		: 'hide',
+	autoLoad		: 'copyright_notice.html',
+	animateTarget	: 'copyright',
+	bodyStyle		: 'padding: 5px;',
+	width			: 600,
+	height			: 500,
+	modal			: false,
+	resizable		: true,
+	draggable		: true,
+	closable		: true,
+	autoScroll		: true
+});
+
+
     /*
      * ================  Simple form  =======================
      */
 var formLogin = new Ext.create('Ext.form.FormPanel', {
-	id: 'frmLogin',
-    url: '../main/main_screen.ejs.php?auth=login&site=<?php echo htmlspecialchars($_SESSION['site_id']); ?>',
-    bodyStyle:'padding:5px 5px 0',
-	frame: false,
-	border: false,
-	doAction: {method: 'POST'},
-    fieldDefaults: { msgTarget: 'side', labelWidth: 300 },
-    defaultType: 'textfield',
-    defaults: {
-        anchor: '100%'
-    },
-
-    items: [{
-        fieldLabel: 'First Name',
-        name: 'first',
-        allowBlank:false
+	id				: 'frmLogin',
+    url				: '../main/main_screen.ejs.php?auth=login&site=<?php echo htmlspecialchars($_SESSION['site_id']); ?>',
+    bodyStyle		:'padding:5px 5px 0',
+	frame			: false,
+	border			: false,
+	doAction		: {method: 'POST'},
+    fieldDefaults	: { msgTarget: 'side', labelWidth: 300 },
+    defaultType		: 'textfield',
+    defaults		: { anchor: '100%' },
+    items: [{ 
+    	xtype: 'textfield', 
+    	ref: '../authPass', 
+    	id: 'authPass', 
+    	hidden: true, 
+    	name: 'authPass', 
+    	value: '' 
     },{
-        fieldLabel: 'Last Name',
-        name: 'last'
-    },{
-        fieldLabel: 'Company',
-        name: 'company'
+        minLength: 3,
+		maxLength: 32, 
+		allowBlank: false, 
+		blankText:'Enter your username', 
+		ref: '../authUser', 
+		id: 'authUser', 
+		name: 'authUser', 
+		validationEvent: false, 
+		fieldLabel: '<?php echo htmlspecialchars( xl('Username'), ENT_NOQUOTES); ?>',
+		minLengthText: 'Username must be at least 3 characters long.' 
+	},{
+        minLength: 4,
+		maxLength: 10, 
+		allowBlank: false,
+		blankText:'Enter your password', 
+		ref: '../clearPass', 
+		inputType: 'password', 
+		id: 'clearPass', 
+		name: 'clearPass', 
+		validationEvent: false,
+		fieldLabel: '<?php echo htmlspecialchars( xl('Password'), ENT_NOQUOTES); ?>',
+		minLengthText: 'Password must be at least 4 characters long.'
+    //},{ 
+    //	xtype: 'combo', 
+    //	id: 'languageChoice', 
+    //	name: 'languageChoice', 
+    //	value: '<?php echo $defaultLangName; ?>', 
+    //	forceSelection: true, 
+    //	fieldLabel: '<?php echo htmlspecialchars( xl('Language'), ENT_NOQUOTES); ?>', 
+    //	editable: false, 
+    //	triggerAction: 'all', 
+    //	mode: 'local', 
+    //	valueField: 'lang_id', 
+    //	hiddenName: 'lang_id', 
+    //	displayField: 'lang_description' 
     }],
     buttons: [{
         text: '<?php echo htmlspecialchars( "Login", ENT_QUOTES); ?>'
@@ -91,8 +138,6 @@ var winLogin = new Ext.create('widget.window', {
     bodyStyle: 'padding: 5px;',
     items: [{ xtype: 'box', width: 483, height: 135, autoEl: {tag: 'img', src: '../../ui_app/logon_header.png'}}, formLogin ]
 });
-
-
 winLogin.show();
    
 });
@@ -101,6 +146,5 @@ winLogin.show();
 </head>
 <body id="login">
 <div id="copyright">MitosEHR v1.0 Development | <a href="javascript:void()" onClick="Ext.getCmp('winCopyright').show();" >Copyright Notice</a></div>
-
 </body>
 </html>
