970fe0525d71e377e72b4030c3a2729eaf5cd979
Gino Rivera <hrivera@mitosehr.org>
2011-02-23 13:19:22 -0400

    new file:   interface/setup/setup.ejs.php

---
+++ interface/setup/setup.ejs.php
@@ -0,0 +1,13 @@
+<?php
+/* The Setup Wizard
+ * 
+ * Description: This script will navigate the user throgh the correct
+ * setup of the software.
+ * 
+ * Author: Gino Rivera Falú
+ * Ver: 0.0.1 
+ */
+
+ echo "MitosEHR Setup Wizard"
+ 
+?>
