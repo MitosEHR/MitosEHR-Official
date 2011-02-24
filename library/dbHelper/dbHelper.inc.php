bf5cb7ed0d42a123bd61eef8340932b688ebc02b
Gino Rivera <hrivera@mitosehr.org>
2011-02-24 15:42:53 -0400

    VEGA: ADOdb Rollout, PHP PDO Rollin. Mush faster DB Manager. Look ma no ADOdb

--- library/dbHelper/dbHelper.inc.php
+++ library/dbHelper/dbHelper.inc.php
@@ -2,7 +2,24 @@
 
 /* db Helper v0.0.1
  * Description: A PDO helper for MitosEHR, containts custom function to manage the database
- * in MitosEHR
+ * in MitosEHR. PDO is new in PHP v5 
+ * 
+ * The PHP Data Objects (PDO) extension defines a lightweight, 
+ * consistent interface for accessing databases in PHP. 
+ * Each database driver that implements the PDO interface can expose database-specific 
+ * features as regular extension functions. Note that you cannot perform any database 
+ * functions using the PDO extension by itself; 
+ * you must use a database-specific PDO driver to access a database server.
+ * 
+ * PDO provides a data-access abstraction layer, which means that, 
+ * regardless of which database you're using, you use the same functions to issue queries 
+ * and fetch data. PDO does not provide a database abstraction; it doesn't rewrite 
+ * SQL or emulate missing features. 
+ * You should use a full-blown abstraction layer if you need that facility.
+ * 
+ * PDO ships with PHP 5.1, and is available as a PECL extension for PHP 5.0; 
+ * PDO requires the new OO features in the core of PHP 5, and so will not 
+ * run with earlier versions of PHP.
  * 
  * Author: Gino Rivera Falu
  * Ver: 0.0.1
