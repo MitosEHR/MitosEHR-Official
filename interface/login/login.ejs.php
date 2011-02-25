7326cba110a822c014131c2e23e8ed86a7ac8fb9
Ernesto's iMac <erodriguez@TPRT001.local>
2011-02-25 00:37:26 -0400

    logout text

--- interface/login/login.ejs.php
+++ interface/login/login.ejs.php
@@ -9,7 +9,7 @@
 ?>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-<TITLE>MitosEHR Logon Screen</TITLE>
+<title>MitosEHR Logon Screen</title>
 
 <script type="text/javascript" src="library/<?php echo $_SESSION['dir']['ext']; ?>/bootstrap.js"></script>
 
