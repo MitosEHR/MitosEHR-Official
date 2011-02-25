844a82a10beebeb28f937308254932fd381baacf
Gino Rivera <hrivera@mitosehr.org>
2011-02-25 11:08:33 -0400

    Comments Commit.

--- index.php
+++ index.php
@@ -12,25 +12,42 @@
  * 
  */
 
+//----------------------------------------------------------------
+// Startup the SESSION
+// This will change in the future. 
+// Maybe implement a SESSION Manager against the database.
+//---------------------------------------------------------------- 
 session_name ( "MitosEHR" );
 session_start();
+
+//----------------------------------------------------------------
+// Startup the registry
+// This containts SESSION Variables to use in the application
+//----------------------------------------------------------------
 include_once("registry.php");
 
+//----------------------------------------------------------------
 // Make the auth process
+//----------------------------------------------------------------
 if ($_SESSION['user']['auth'] == TRUE){
 	//----------------------------------------------------------------
+	// Load the i18n Library
 	// Load the main screen
 	//----------------------------------------------------------------
 	include_once("library/I18n/I18n.inc.php");
 	include_once("interface/main/main_screen.ejs.php");
+	
+//----------------------------------------------------------------
+// Make the logon process or Setup process
+//----------------------------------------------------------------
 } else {
 	//----------------------------------------------------------------
-	// Browse the site dir
+	// Browse the site dir first
 	//----------------------------------------------------------------
 	$d = dir("sites/");
 	while (false !== ($entry = $d->read())) { if ( $entry != "." && $entry != ".."){ $count++; } }
 	//----------------------------------------------------------------
-	// If no directory is found inside sites dir run the setup wizard
+	// If no directory is found inside sites dir run the setup wizard,
 	// if a directory is found inside sites dir run the logon screen
 	//----------------------------------------------------------------
 	if( $count <= 0){
