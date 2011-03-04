f02729dcc4f2064333ccb91727ac3bfc34766870
Ernesto Rodriguez <Ernesto@Omar-Rodriguezs-MacBook-Pro.local>
2011-03-03 20:10:28 -0500

    grid role sql

--- interface/administration/roles/data_read.ejs.php
+++ interface/administration/roles/data_read.ejs.php
@@ -22,24 +22,26 @@ $count = 0;
 // and execute the apropriate SQL statement
 // query all permissions and left join with currRole values
 // *************************************************************************************
-$currRole = ($_REQUEST["start"] == null) ? 5 : $_REQUEST["role_id"];
+$currRole = ($_REQUEST["start"] == null) ? 6 : $_REQUEST["role_id"];
 
-$sql = "SELECT acl_permissions.id,
+$sql = "SELECT acl_roles.role_name,
+			   acl_permissions.id,
 			   acl_permissions.perm_key,
 			   acl_permissions.perm_name,
 			   acl_role_perms.role_id,
 			   acl_role_perms.perm_id,
 			   acl_role_perms.value
-		  FROM acl_permissions
-  	 LEFT JOIN acl_role_perms
-	 	    ON acl_permissions.id = acl_role_perms.perm_id
-	 	 LIKE role_id = '$currRole' 
-  		 ORDER BY perm_name";
+		  FROM (acl_role_perms
+  	 LEFT JOIN acl_roles ON acl_role_perms.role_id = acl_roles.id)
+  	 RIGHT JOIN acl_permissions ON acl_role_perms.perm_id = acl_permissions.id
+
+  		 ORDER BY role_name DESC";
 	$buff = "";
 	foreach (sqlStatement($sql) as $urow) {
 		$count++;
 		$buff .= "{";
 		$buff .= " id: '" . $urow['id'] . "',";
+		$buff .= " role_name: '" . dataEncode( $urow['role_name'] ) . "',";
 		$buff .= " perm_key: '" . $urow['perm_key'] . "',";
 		$buff .= " perm_name: '" . dataEncode( $urow['perm_name'] ) . "',";
 		$buff .= " role_id: '" . $urow['role_id'] . "',";
