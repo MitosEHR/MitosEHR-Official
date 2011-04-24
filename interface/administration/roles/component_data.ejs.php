bcd1082efdc7a98172970ff295f65fce92c18809
Ernesto J Rodriguez <ernesto@ernesto-linux-laptop.(none)>
2011-04-24 13:11:10 -0400

    Sql and json print fixed

--- interface/administration/roles/component_data.ejs.php
+++ interface/administration/roles/component_data.ejs.php
@@ -8,10 +8,14 @@
 //--------------------------------------------------------------------------------------------------------------------------
 session_name ( "MitosEHR" );
 session_start();
+
 include_once("../../../library/dbHelper/dbHelper.inc.php");
 include_once("../../../library/acl/class.inc.php");
+$mitos_db = new dbHelper();
+
 // Count records variable
 $count = 0;
+$buff = "";
 // *************************************************************************************
 // Deside what to do with the $_GET['task']
 // *************************************************************************************
@@ -20,23 +24,22 @@ switch ($_GET['task']) {
 	// Data for for Role List
 	// *********************************************************************************
 	case "roles":
-	// *********************************************************************************
-	// get full list of roles
-	// *********************************************************************************
-	$sql = "SELECT * FROM acl_roles ORDER BY role_name ASC";
-	$buff = "";
-	foreach (sqlStatement($sql) as $urow) {
-		$count++;
-		$buff .= "{";
-		$buff .= " id: '" . $urow['id'] . "',";
-		$buff .= " role_name: '" . $urow['role_name'] . "'}," . chr(13);
-	}
-	$buff = substr($buff, 0, -2); // Delete the last comma and clear the buff.
-	echo $_GET['callback'] . '({';
-	echo "totalCount: " . $count . ", " . chr(13);
-	echo "row: [" . chr(13);
-	echo $buff;
-	echo "]})" . chr(13);
+		// *****************************************************************************
+		// get full list of roles
+		// *****************************************************************************
+		$mitos_db->setSQL("SELECT * FROM acl_roles ORDER BY role_name ASC");
+		foreach ($mitos_db->execStatement() as $urow) {
+			$count++;
+			$buff .= '{';
+			$buff .= ' "id": "' . $urow['id'] . '",';
+			$buff .= ' "role_name": "' . $urow['role_name'] . '"},' . chr(13);
+		}
+		$buff = substr($buff, 0, -2); // Delete the last comma and clear the buff.
+		echo '{';
+		echo '"totals": "' . $count . '", ' . chr(13);
+		echo '"row": [' . chr(13);
+		echo $buff;
+		echo ']}' . chr(13); 
 	break;
 }
 ?>
