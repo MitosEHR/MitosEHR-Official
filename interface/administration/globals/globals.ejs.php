d248eedef1cb764bc25b91debe414b5bdb4e58c8
Ernesto J Rodriguez <ernesto@ernesto-linux-laptop.(none)>
2011-04-27 13:52:05 -0400

    Appearance tab fields added

--- interface/administration/globals/globals.ejs.php
+++ interface/administration/globals/globals.ejs.php
@@ -25,24 +25,127 @@ $_SESSION['site']['flops'] = 0;
 <script type="text/javascript">
 
 Ext.onReady(function(){
-
-
-var globalFormPanel = Ext.create('Ext.form.Panel', {
+	//**************************************************************************
+	// Dummy Store
+	//**************************************************************************
+	Ext.namespace('Ext.data');
+	Ext.data.dummy = [
+	    ['Option 1', 'Option 1'],
+	    ['Option 2', 'Option 2'],
+	    ['Option 3', 'Option 3'],
+	    ['Option 4', 'Option 4']
+	];
+	var dummyStore = new Ext.data.ArrayStore({
+	    fields: ['name', 'value'],
+	    data : Ext.data.dummy
+	});
+	//**************************************************************************
+	// Dummy Store
+	//**************************************************************************
+	var globalFormPanel = Ext.create('Ext.form.Panel', {
 		border: false,
 		layout: 'fit',
-        fieldDefaults: { labelAlign: 'top', msgTarget: 'side' },
+		autoScroll:true,
+        fieldDefaults: { msgTarget: 'side', labelWidth: 200, width: 500 },
         defaults: { anchor: '100%' },
         items: [{
             xtype:'tabpanel',
             activeTab: 0,
-            defaults:{bodyStyle:'padding:10px'},
+            defaults:{bodyStyle:'padding:10px', autoScroll:true },
             items:[{
                 title:'Appearance',
-                defaults: {width: 230},
-                defaultType: 'textfield',
-                items: [
-				
-				]
+                defaults: {anchor: '100%'},
+                items: [{
+					xtype		: 'combo',
+					fieldLabel	: '<?php i18n('Main Top Pane Screen'); ?>',
+					name		: 'mainPaneScreen',
+					id			: 'mainPaneScreen',
+					displayField: 'name',
+					valueField	: 'value',
+					editable	: false,
+					store		: dummyStore
+				},{
+					xtype		: 'combo',
+					fieldLabel	: '<?php i18n('Layout Style'); ?>',
+					name		: 'layoutStyle',
+					id			: 'layoutStyle',
+					displayField: 'name',
+					valueField	: 'value',
+					editable	: false,
+					store		: dummyStore
+				},{
+					xtype		: 'combo',
+					fieldLabel	: '<?php i18n('Theme'); ?>',
+					name		: 'theme',
+					id			: 'theme',
+					displayField: 'name',
+					valueField	: 'value',
+					editable	: false,
+					store		: dummyStore
+				},{
+					xtype		: 'textfield',
+					fieldLabel	: '<?php i18n('Navigation Area Width'); ?>',
+					name		: 'navWidth',
+					id			: 'navWidth'
+				},{
+					xtype		: 'textfield',
+					fieldLabel	: '<?php i18n('Application Title'); ?>',
+					name		: 'appTitle',
+					id			: 'appTitle'
+				},{
+					xtype		: 'combo',
+					fieldLabel	: '<?php i18n('New Patient Form'); ?>',
+					name		: 'newPatientForm',
+					id			: 'newPatientForm', 
+					displayField: 'name',
+					valueField	: 'value',
+					editable	: false,
+					store		: dummyStore
+				},{
+					xtype		: 'combo',
+					fieldLabel	: '<?php i18n('Patient Search Resuls Style'); ?>',
+					name		: 'patientSearchStyle',
+					id			: 'patientSearchStyle',
+					displayField: 'name',
+					valueField	: 'value',
+					editable	: false,
+					store		: dummyStore
+				},{
+					xtype		: 'checkbox',
+					fieldLabel	: '<?php i18n('Tall Navigation Area'); ?>',
+					name		: 'tallNavArea',
+					id			: 'tallNavArea'
+				},{
+					xtype		: 'checkbox',
+					fieldLabel	: '<?php i18n('Navigation Area Visit Form'); ?>',
+					name		: 'navAreaVisitForm',
+					id			: 'navAreaVisitForm'
+				},{
+					xtype		: 'checkbox',
+					fieldLabel	: '<?php i18n('Simplified Demographics'); ?>',
+					name		: 'simpleDemo',
+					id			: 'simpleDemo'
+				},{
+					xtype		: 'checkbox',
+					fieldLabel	: '<?php i18n('Simplified Prescriptions'); ?>',
+					name		: 'simplePrescription',
+					id			: 'simplePrescription'
+				},{
+					xtype		: 'checkbox',
+					fieldLabel	: '<?php i18n('Simplified Co-Pay'); ?>',
+					name		: 'simpleCoPay',
+					id			: 'simpleCoPay'
+				},{
+					xtype		: 'checkbox',
+					fieldLabel	: '<?php i18n('User Charges Panel'); ?>',
+					name		: 'userChargesPanel',
+					id			: 'userChargesPanel'
+				},{
+					xtype		: 'textfield',
+					fieldLabel	: '<?php i18n('Online Support Link'); ?>',
+					name		: 'supportLink',
+					id			: 'supportLink'
+				}]
             },{
                 title:'Locale',
                 defaults: {width: 230},
@@ -99,7 +202,7 @@ var globalFormPanel = Ext.create('Ext.form.Panel', {
 			  	items: [{
 					id        : 'addAddressbook',
 				    text      : '<?php i18n("Save Configuration"); ?>',
-				    iconCls   : 'icoAddressBook',
+				    iconCls   : 'icoSave',
 				    handler   : function(){
 						//*** SAVE FUNCTION - TODO ***//
 				    }
