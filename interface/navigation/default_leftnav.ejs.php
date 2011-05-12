4ac667e88a628eff2a417941f949b885466d2462
Gino Rivera <hrivera@gi-technologies.com>
2011-05-11 23:14:32 -0400

    Flop Fixes

--- interface/navigation/default_leftnav.ejs.php
+++ interface/navigation/default_leftnav.ejs.php
@@ -8,6 +8,11 @@ include_once("../../library/dbHelper/dbHelper.inc.php");
 include_once("../../library/I18n/I18n.inc.php");
 require_once("../../repository/dataExchange/dataExchange.inc.php");
 
+//******************************************************************************
+// Reset session count 10 secs = 1 Flop
+//******************************************************************************
+$_SESSION['site']['flops'] = 0;
+
 // *************************************************************************************
 // Renders the items of the navigation panel
 // Default Nav Data
