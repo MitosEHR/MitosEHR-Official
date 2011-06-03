6306ed59a4527a805a6579b3d39ea3170aec50b8
vela1606 <Ernesto J Rodriguez>
2011-06-03 14:52:03 -0400

    few classes added

---
+++ repository/ext_classes/mitos/FormPanel.js
@@ -0,0 +1,17 @@
+
+Ext.define('Ext.mitos.FormPanel',{
+	extend      : 'Ext.form.Panel',
+    alias       : 'mitos.formpanel',
+    bodyStyle   : 'padding: 5px;',
+	autoWidth   : true,
+	border      : false,
+
+    initComponent: function(){	
+    	var me = this;
+
+    	Ext.apply(this, {
+					});
+		me.callParent(arguments);
+		
+	} // end initComponent
+});
---
+++ repository/ext_classes/mitos/Window.js
@@ -0,0 +1,19 @@
+
+Ext.define('Ext.mitos.Window',{
+	extend      : 'Ext.window.Window',
+    alias       : 'mitos.window',
+
+    initComponent: function(){	
+    	var me = this;
+
+    	Ext.apply(this, {
+			autoHeight	: true,
+			modal		: true,
+			resizable	: false,
+			autoScroll	: false,
+			closeAction	: 'hide',
+			renderTo	: document.body		});
+		me.callParent(arguments);
+		
+	} // end initComponent
+});
