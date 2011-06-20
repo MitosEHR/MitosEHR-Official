d8e6257ea698eaf5805349bd5da804348ddbe330
vela1606 <Ernesto J Rodriguez>
2011-06-20 12:19:57 -0400

    id changed to hrefTarget

--- interface/navigation/default_leftnav.ejs.php
+++ interface/navigation/default_leftnav.ejs.php
@@ -22,23 +22,23 @@ $buff = "[" . chr(13);
 // -------------------------------------
 // Dashboard
 // -------------------------------------
-$buff .= '{ "text":"' . i18n('Dashboard', 'r') . '", "leaf":true, "cls":"file", "id":"dashboard/dashboard.ejs.php"},' . chr(13);
-$buff .= '{ "text":"' . i18n('Messages', 'r') . '", "leaf":true, "cls":"file", "id":"messages/messages.ejs.php"},' . chr(13);
+$buff .= '{ "text":"' . i18n('Dashboard', 'r') . '", "leaf":true, "cls":"file", "hrefTarget":"dashboard/dashboard.ejs.php"},' . chr(13);
+$buff .= '{ "text":"' . i18n('Messages', 'r') . '", "leaf":true, "cls":"file", "hrefTarget":"messages/messages.ejs.php"},' . chr(13);
 // -------------------------------------
 // Patient
 // -------------------------------------
 $buff .= '{"text":"' . i18n('Patient - TODO', 'r') . '", "cls":"folder", "expanded": true,' . chr(13);
 	$buff .= '"children": [' . chr(13); // ^ Folder
-	$buff .= '{"text":"' . i18n('New Patient - TODO', 'r') . '", "leaf":true, "cls":"file", "id":"patient_file/new/new_patient.ejs.php"},' . chr(13);
-	$buff .= '{"text":"' . i18n('Patient File - TODO', 'r') . '", "leaf":true, "cls":"file", "id":"patient_file/patient_file.ejs.php"},' . chr(13);
+	$buff .= '{"text":"' . i18n('New Patient - TODO', 'r') . '", "leaf":true, "cls":"file", "hrefTarget":"patient_file/new/new_patient.ejs.php"},' . chr(13);
+	$buff .= '{"text":"' . i18n('Patient File - TODO', 'r') . '", "leaf":true, "cls":"file", "hrefTarget":"patient_file/patient_file.ejs.php"},' . chr(13);
 	// -------------------------------------
 	// Patient
 	// -------------------------------------
 	$buff .= '{"text":"' . i18n('Visits - TODO', 'r') . '", "cls":"folder", ' . chr(13);
 		$buff .= '"children": [' . chr(13); // ^ Folder
-		$buff .= '{"text":"' . i18n('Create Visit - TODO', 'r') . '", "leaf":true, "cls":"file", "id":"patient_file/visit/create.ejs.php"},' . chr(13);
-		$buff .= '{"text":"' . i18n('Current Visit - TODO', 'r') . '", "leaf":true, "cls":"file", "id":"patient_file/visit/current.ejs.php"},' . chr(13);
-		$buff .= '{"text":"' . i18n('Visit History - TODO', 'r') . '", "leaf":true, "cls":"file", "id":"patient_file/visit/history.ejs.php"}' . chr(13);
+		$buff .= '{"text":"' . i18n('Create Visit - TODO', 'r') . '", "leaf":true, "cls":"file", "hrefTarget":"patient_file/visit/create.ejs.php"},' . chr(13);
+		$buff .= '{"text":"' . i18n('Current Visit - TODO', 'r') . '", "leaf":true, "cls":"file", "hrefTarget":"patient_file/visit/current.ejs.php"},' . chr(13);
+		$buff .= '{"text":"' . i18n('Visit History - TODO', 'r') . '", "leaf":true, "cls":"file", "hrefTarget":"patient_file/visit/history.ejs.php"}' . chr(13);
 	$buff .= ']}' . chr(13);
 $buff .= ']},' . chr(13);
 // -------------------------------------
@@ -46,25 +46,25 @@ $buff .= ']},' . chr(13);
 // -------------------------------------
 $buff .= '{"text":"' . i18n('Administration', 'r') . '", "cls":"folder", "expanded": true, ' . chr(13);
 	$buff .= '"children": [' . chr(13); // ^ Folder
-	$buff .= '{"text":"' . i18n('Global Settings', 'r') . '", "leaf":true, "cls":"file", "id":"administration/globals/globals.ejs.php"},' . chr(13);
-	$buff .= '{"text":"' . i18n('Facilities', 'r') . '", "leaf":true, "cls":"file", "id":"administration/facilities/facilities.ejs.php"},' . chr(13);
-	$buff .= '{"text":"' . i18n('Users', 'r') . '", "leaf":true, "cls":"file", "id":"administration/users/users.ejs.php"},' . chr(13);
-	$buff .= '{"text":"' . i18n('Practice', 'r') . '", "leaf":true, "cls":"file", "id":"administration/practice/practice.ejs.php"},' . chr(13);
-	$buff .= '{"text":"' . i18n('Roles', 'r') . '", "leaf":true, "cls":"file", "id":"administration/roles/roles.ejs.php"},' . chr(13);
-	$buff .= '{"text":"' . i18n('Layouts', 'r') . '", "leaf":true, "cls":"file", "id":"administration/layout/layout.ejs.php"},' . chr(13);
-	$buff .= '{"text":"' . i18n('Lists', 'r') . '", "leaf":true, "cls":"file", "id":"administration/lists/lists.ejs.php"},' . chr(13);
-    $buff .= '{"text":"' . i18n('Event Log', 'r') . '", "leaf":true, "cls":"file", "id":"administration/log/log.ejs.php"}' . chr(13);
+	$buff .= '{"text":"' . i18n('Global Settings', 'r') . '", "leaf":true, "cls":"file", "hrefTarget":"administration/globals/globals.ejs.php"},' . chr(13);
+	$buff .= '{"text":"' . i18n('Facilities', 'r') . '", "leaf":true, "cls":"file", "hrefTarget":"administration/facilities/facilities.ejs.php"},' . chr(13);
+	$buff .= '{"text":"' . i18n('Users', 'r') . '", "leaf":true, "cls":"file", "hrefTarget":"administration/users/users.ejs.php"},' . chr(13);
+	$buff .= '{"text":"' . i18n('Practice', 'r') . '", "leaf":true, "cls":"file", "hrefTarget":"administration/practice/practice.ejs.php"},' . chr(13);
+	$buff .= '{"text":"' . i18n('Roles', 'r') . '", "leaf":true, "cls":"file", "hrefTarget":"administration/roles/roles.ejs.php"},' . chr(13);
+	$buff .= '{"text":"' . i18n('Layouts', 'r') . '", "leaf":true, "cls":"file", "hrefTarget":"administration/layout/layout.ejs.php"},' . chr(13);
+	$buff .= '{"text":"' . i18n('Lists', 'r') . '", "leaf":true, "cls":"file", "hrefTarget":"administration/lists/lists.ejs.php"},' . chr(13);
+    $buff .= '{"text":"' . i18n('Event Log', 'r') . '", "leaf":true, "cls":"file", "hrefTarget":"administration/log/log.ejs.php"}' . chr(13);
 $buff .= ']},' . chr(13);
 // -------------------------------------
 // Administration
 // -------------------------------------
 $buff .= '{"text":"' . i18n('Miscellaneous', 'r') . '", "cls":"folder", "expanded": true, ' . chr(13);
 	$buff .= '"children": [' . chr(13); // ^ Folder
-    $buff .= '{"text":"' . i18n('Web Search', 'r') . '", "leaf":true, "cls":"file", "id":"miscellaneous/websearch/websearch.ejs.php"},' . chr(13);
-	$buff .= '{"text":"' . i18n('Address Book', 'r') . '", "leaf":true, "cls":"file", "id":"miscellaneous/addressbook/addressbook.ejs.php"},' . chr(13);
-	$buff .= '{"text":"' . i18n('Office Notes', 'r') . '", "leaf":true, "cls":"file", "id":"miscellaneous/office_notes/office_notes.ejs.php"},' . chr(13);
-	$buff .= '{"text":"' . i18n('My Settings', 'r') . '", "leaf":true, "cls":"file", "id":"miscellaneous/my_settings/my_settings.ejs.php"},' . chr(13);
-	$buff .= '{"text":"' . i18n('My Account', 'r') . '", "leaf":true, "cls":"file", "id":"miscellaneous/my_account/my_account.ejs.php"}' . chr(13);
+    $buff .= '{"text":"' . i18n('Web Search', 'r') . '", "leaf":true, "cls":"file", "hrefTarget":"miscellaneous/websearch/websearch.ejs.php"},' . chr(13);
+	$buff .= '{"text":"' . i18n('Address Book', 'r') . '", "leaf":true, "cls":"file", "hrefTarget":"miscellaneous/addressbook/addressbook.ejs.php"},' . chr(13);
+	$buff .= '{"text":"' . i18n('Office Notes', 'r') . '", "leaf":true, "cls":"file", "hrefTarget":"miscellaneous/office_notes/office_notes.ejs.php"},' . chr(13);
+	$buff .= '{"text":"' . i18n('My Settings', 'r') . '", "leaf":true, "cls":"file", "hrefTarget":"miscellaneous/my_settings/my_settings.ejs.php"},' . chr(13);
+	$buff .= '{"text":"' . i18n('My Account', 'r') . '", "leaf":true, "cls":"file", "hrefTarget":"miscellaneous/my_account/my_account.ejs.php"}' . chr(13);
 $buff .= ']},' . chr(13);
 // -------------------------------------
 // Test Folder
