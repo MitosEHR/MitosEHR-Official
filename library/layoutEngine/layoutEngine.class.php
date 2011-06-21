9bf46297d1a5c4311a4a94197720cec593fc00da
Gino Rivera <hrivera@gi-technologies.com>
2011-06-21 00:31:13 -0400

    layoutEngine.class.php -> When this is finish will need seriuos fine tunning.

--- library/layoutEngine/layoutEngine.class.php
+++ library/layoutEngine/layoutEngine.class.php
@@ -29,6 +29,21 @@ class layoutEngine {
 		}
 	}
 
+	function getForm($form_id="Demographics"){
+		$sql = "SELECT 
+					layout_options.*, list_options.title AS listDesc
+				FROM
+  					layout_options
+				LEFT OUTER JOIN 
+					list_options
+				ON 
+					layout_options.list_id = list_options.option_id
+				WHERE
+  					layout_options.form_id = '". $form_id . "'
+				ORDER BY
+  					layout_options.group_order, layout_options.seq";
+	}
+
 	//**********************************************************************
 	// switchTF
 	// This function will write the Sencha ExtJS v4 code, in two modes
@@ -36,6 +51,53 @@ class layoutEngine {
 	// F = Fields: Will create the form with fields
 	//**********************************************************************
 	function switchTF($v = "F"){ $this->switcher = $v; } 
+	
+	//**********************************************************************
+	// formPanel
+	//
+	// This creates the Sencha form object, in OOP
+	//
+	// Parameters:
+	// $start: S for start the form, or E to end the form
+	// $formPanel: The name of the form panel object
+	// $title: The title of the form panel object
+	// $url: Where te results will be send to.
+	//**********************************************************************
+	function formPanel($start="S", $formPanel="formPanel", $title, $url){
+		if($start=="S"){
+			echo "panel." . $formPanel . " = Ext.create('Ext.form.Panel', {
+    					title: '" . $title . "',
+    					labelWidth: 80,
+    					url: '" . $url . "',
+	    				frame: true,
+    					bodyStyle: 'padding: 5px',
+    					width: '100%',
+    					layout: 'column',
+    					defaults: {
+    	    				bodyPadding: 4
+	    				},";
+    	}
+		if ($start=="E") echo "});";
+	}
+	
+	//**********************************************************************
+	// Create the fieldset
+	//**********************************************************************
+	function formFieldset($start="S", $fieldsetName, $column=1){
+		$cpos = $column * 0.5;
+		if($start=="S"){
+			echo "items: [{
+        				xtype:'fieldset',
+        				columnWidth: ".$cpos.",
+        				title: '".$fieldsetName."',
+        				collapsible: true,
+        				defaults: {anchor: '100%'},
+        				layout: 'anchor',";	
+		}
+		if($start=="E"){
+			echo "}]";
+		}
+	}
 	
 	
 }
