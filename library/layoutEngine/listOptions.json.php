5758f52fc891d7b688e8e58ed55e3a03280a2d61
Gino Rivera <hrivera@gi-technologies.com>
2011-06-21 03:11:41 -0400

    listOption.json.php introduced -> This file can be used by the MitosEHR application, to request from list_option table.

---
+++ library/layoutEngine/listOptions.json.php
@@ -0,0 +1,65 @@
+<?php
+//--------------------------------------------------------------------------------------------------------------------------
+// listOptions.json.php
+// 
+// This is a json injector, to deal with the table list_options
+// depending on the params: filter option passed trough ExtJS v4 dataStore
+// it will return the list
+//
+// v0.0.1
+// Under GPLv3 License
+//
+// Integrated by: Gi Technologies. in 2011
+//
+// Remember, this file is called via the Framework Store, this is the AJAX thing.
+//--------------------------------------------------------------------------------------------------------------------------
+
+session_name ( "MitosEHR" );
+session_start();
+session_cache_limiter('private');
+
+// **************************************************************************************
+// filter params passed by ExtJS Data Store
+// **************************************************************************************
+$filter = $_REQUEST['filter'];
+
+include_once($_SESSION['site']['root']."/library/dbHelper/dbHelper.inc.php");
+
+// **************************************************************************************
+// Reset session count 10 secs = 1 Flop
+// **************************************************************************************
+$_SESSION['site']['flops'] = 0;
+
+// **************************************************************************************
+// Database class instance
+// **************************************************************************************
+$mitos_db = new dbHelper();
+
+if ($_SESSION['lang']['code'] == "en_US") { // If the selected language is English, do not translate
+		$mitos_db->setSQL("SELECT 
+								*
+							FROM 
+								list_options 
+							WHERE 
+								list_id = '".$filter."' 
+							ORDER BY title, seq");
+} else {
+	// Use and sort by the translated list name.
+	$mitos_db->setSQL("SELECT 
+							*, 
+							IF(LENGTH(ld.definition),ld.definition,lo.title) AS title 
+						FROM list_options AS lo 
+							LEFT JOIN lang_constants AS lc ON lc.constant_name = lo.title 
+							LEFT JOIN lang_definitions AS ld ON ld.cons_id = lc.cons_id AND ld.lang_id = '$lang_id' 
+						WHERE 
+							lo.list_id = '".$filter."' 
+						ORDER BY 
+							IF(LENGTH(ld.definition),ld.definition,lo.title), lo.seq");
+}
+$total = $mitos_db->rowCount();
+$rows = array();
+foreach ($mitos_db->execStatement() as $row) {
+	array_push($rows, $row);
+}
+print(json_encode(array('totals'=>$total,'row'=>$rows)));
+?>
