d35fb9a5237394904045bca1d48a98e3f7bd2fac
Gino Rivera <hrivera@gi-technologies.com>
2011-06-22 22:28:32 -0400

    Class and Classes

--- interface/administration/facilities/facilities.ejs.php
+++ interface/administration/facilities/facilities.ejs.php
@@ -29,8 +29,7 @@ Ext.onReady(function() {
 			'Ext.mitos.CRUDStore',
 			'Ext.mitos.GridPanel',
 			'Ext.mitos.TopRenderPanel',
-			'Ext.mitos.SaveCancelWindow',
-			'Ext.mitos.MitosApplication'
+			'Ext.mitos.SaveCancelWindow'
 		],
 		initComponent: function(){
 		    /** @namespace Ext.QuickTips */
