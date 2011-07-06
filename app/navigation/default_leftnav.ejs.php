9f7e6557dc4c0d5006fa6d84c4a43b73c0a2c7bb
vela1606 <vela1606@gmail.com>
2011-07-06 11:31:23 -0400

    folders arrays separated, easier to manage array values if need it

--- app/navigation/default_leftnav.ejs.php
+++ app/navigation/default_leftnav.ejs.php
@@ -3,9 +3,9 @@ session_name ( "MitosEHR" );
 session_start();
 session_cache_limiter('private');
 include_once($_SESSION['site']['root']."/classes/I18n.class.php");
-//******************************************************************************
+// *************************************************************************************
 // Reset session count 10 secs = 1 Flop
-//******************************************************************************
+// *************************************************************************************
 $_SESSION['site']['flops'] = 0;
 // *************************************************************************************
 // Renders the items of the navigation panel
@@ -15,60 +15,83 @@ $nav = array(
     array( 'text' => i18n('Dashboard', 'r'), 'leaf' => true, 'cls' => 'file', 'iconCls' => 'icoDash',      'hrefTarget' => 'dashboard/dashboard.ejs.php' ),
     array( 'text' => i18n('Calendar', 'r'),  'leaf' => true, 'cls' => 'file', 'iconCls' => 'icoCalendar',  'hrefTarget' => 'calendar/calendar.ejs.php' ),
     array( 'text' => i18n('Messages', 'r'),  'leaf' => true, 'cls' => 'file', 'iconCls' => 'mail',         'hrefTarget' => 'messages/messages.ejs.php' ),
-    // Patient Folder
-    array( 'text' => i18n('Patient', 'r'), 'cls' => 'folder', 'expanded' => true, 'children' =>
-        array(
-            array( 'text' => i18n('New Patient', 'r'),     'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'patient_file/new/new_patient.ejs.php' ),
-            array( 'text' => i18n('Patient Summary', 'r'), 'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'patient_file/summary/summary.ejs.php' ),
-            // Patient Visits Folder
-            array( 'text' => i18n('Visits', 'r'), 'cls' => 'folder', 'expanded' => true, 'children' =>
-                array(
-                    array( 'text' => i18n('Create Visit', 'r'),    'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'patient_file/visits/visit_create/visit_create.ejs.php' ),
-                    array( 'text' => i18n('Current Visit', 'r'),   'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'patient_file/visits/visit_current/visit_current.ejs.php' ),
-                    array( 'text' => i18n('Visit History', 'r'),   'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'patient_file/visits/visit_history/visit_history.ejs.php' ),
-                )
-            ), // End Visits Folder
-        )
-    ), // End Patient Folder
-    // Fees Folder
-    array( 'text' => i18n('Fees', 'r'), 'cls' => 'folder', 'expanded' => true, 'children' =>
-        array(
-            array( 'text' => i18n('Billing', 'r'),         'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'fees/billing/billing.ejs.php' ),
-            array( 'text' => i18n('Checkout', 'r'),        'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'fees/checkout/checkout.ejs.php' ),
-            array( 'text' => i18n('Fees Sheet', 'r'),      'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'fees/fees_sheet/fees_sheet.ejs.php' ),
-            array( 'text' => i18n('Payments', 'r'),        'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'fees/payments/payments.ejs.php' ),
-        )
-    ), // End Fees Folder
-    // Administration Folder
-    array( 'text' => i18n('Administration', 'r'), 'cls' => 'folder', 'expanded' => true, 'children' =>
-        array(
-            array( 'text' => i18n('Global Settings', 'r'), 'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'administration/globals/globals.ejs.php' ),
-            array( 'text' => i18n('Facilities', 'r'),      'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'administration/facilities/facilities.ejs.php' ),
-            array( 'text' => i18n('Users', 'r'),           'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'administration/users/users.ejs.php' ),
-            array( 'text' => i18n('Practice', 'r'),        'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'administration/practice/practice.ejs.php' ),
-            array( 'text' => i18n('Services', 'r'),        'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'administration/services/services.ejs.php' ),
-            array( 'text' => i18n('Roles', 'r'),           'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'administration/roles/roles.ejs.php' ),
-            array( 'text' => i18n('Layouts', 'r'),         'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'administration/layout/layout.ejs.php' ),
-            array( 'text' => i18n('Lists', 'r'),           'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'administration/lists/lists.ejs.php' ),
-            array( 'text' => i18n('Event Log', 'r'),       'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'administration/log/log.ejs.php' ),
-        )
-    ), // End Administration Folder
-    // Miscellaneous Folder
-    array( 'text' => i18n('Miscellaneous', 'r'), 'cls' => 'folder', 'expanded' => true, 'children' =>
-        array(
-            array( 'text' => i18n('Web Search', 'r'),      'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'miscellaneous/websearch/websearch.ejs.php' ),
-            array( 'text' => i18n('Address Book', 'r'),    'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'miscellaneous/addressbook/addressbook.ejs.php' ),
-            array( 'text' => i18n('Office Notes', 'r'),    'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'miscellaneous/office_notes/office_notes.ejs.php' ),
-            array( 'text' => i18n('My Settings', 'r'),     'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'miscellaneous/my_settings/my_settings.ejs.php' ),
-            array( 'text' => i18n('My Account', 'r'),      'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'miscellaneous/my_account/my_account.ejs.php' ),
-        )
-    ), // End Fees Folder
-    // Test Folder
-    array( 'text' => i18n('Test', 'r'), 'cls' => 'folder', 'expanded' => true, 'children' =>
-        array(
-        )
-    ), // End Test Folder
 );
-// Lets json encode and print $nav array
+// *************************************************************************************
+// Patient Folder
+// *************************************************************************************
+$patient_folder = array( 'text' => i18n('Patient', 'r'), 'cls' => 'folder', 'expanded' => true, 'children' =>
+    array(
+        array( 'text' => i18n('New Patient', 'r'),     'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'patient_file/new/new_patient.ejs.php' ),
+        array( 'text' => i18n('Patient Summary', 'r'), 'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'patient_file/summary/summary.ejs.php' ),
+        // Patient Visits Folder
+        array( 'text' => i18n('Visits', 'r'), 'cls' => 'folder', 'expanded' => true, 'children' =>
+            array(
+                array( 'text' => i18n('Create Visit', 'r'),    'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'patient_file/visits/visit_create/visit_create.ejs.php' ),
+                array( 'text' => i18n('Current Visit', 'r'),   'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'patient_file/visits/visit_current/visit_current.ejs.php' ),
+                array( 'text' => i18n('Visit History', 'r'),   'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'patient_file/visits/visit_history/visit_history.ejs.php' ),
+            )
+        ), // End Visits Folder
+    )
+);
+// *************************************************************************************
+// Fees Folder
+// *************************************************************************************
+$fees_folder = array( 'text' => i18n('Fees', 'r'), 'cls' => 'folder', 'expanded' => true, 'children' =>
+    array(
+        array( 'text' => i18n('Billing', 'r'),         'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'fees/billing/billing.ejs.php' ),
+        array( 'text' => i18n('Checkout', 'r'),        'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'fees/checkout/checkout.ejs.php' ),
+        array( 'text' => i18n('Fees Sheet', 'r'),      'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'fees/fees_sheet/fees_sheet.ejs.php' ),
+        array( 'text' => i18n('Payments', 'r'),        'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'fees/payments/payments.ejs.php' ),
+    )
+);
+// *************************************************************************************
+// Administration Folder
+// *************************************************************************************
+$admin_folder = array( 'text' => i18n('Administration', 'r'), 'cls' => 'folder', 'expanded' => true, 'children' =>
+    array(
+        array( 'text' => i18n('Global Settings', 'r'), 'leaf' => true, 'cls' => 'file', 'disable' => true, 'hrefTarget' => 'administration/globals/globals.ejs.php' ),
+        array( 'text' => i18n('Facilities', 'r'),      'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'administration/facilities/facilities.ejs.php' ),
+        array( 'text' => i18n('Users', 'r'),           'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'administration/users/users.ejs.php' ),
+        array( 'text' => i18n('Practice', 'r'),        'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'administration/practice/practice.ejs.php' ),
+        array( 'text' => i18n('Services', 'r'),        'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'administration/services/services.ejs.php' ),
+        array( 'text' => i18n('Roles', 'r'),           'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'administration/roles/roles.ejs.php' ),
+        array( 'text' => i18n('Layouts', 'r'),         'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'administration/layout/layout.ejs.php' ),
+        array( 'text' => i18n('Lists', 'r'),           'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'administration/lists/lists.ejs.php' ),
+        array( 'text' => i18n('Event Log', 'r'),       'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'administration/log/log.ejs.php' ),
+    )
+);
+// *************************************************************************************
+// Miscellaneous Folder
+// *************************************************************************************
+$misc_folder = array( 'text' => i18n('Miscellaneous', 'r'), 'cls' => 'folder', 'expanded' => true, 'children' =>
+    array(
+        array( 'text' => i18n('Web Search', 'r'),      'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'miscellaneous/websearch/websearch.ejs.php' ),
+        array( 'text' => i18n('Address Book', 'r'),    'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'miscellaneous/addressbook/addressbook.ejs.php' ),
+        array( 'text' => i18n('Office Notes', 'r'),    'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'miscellaneous/office_notes/office_notes.ejs.php' ),
+        array( 'text' => i18n('My Settings', 'r'),     'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'miscellaneous/my_settings/my_settings.ejs.php' ),
+        array( 'text' => i18n('My Account', 'r'),      'leaf' => true, 'cls' => 'file', 'hrefTarget' => 'miscellaneous/my_account/my_account.ejs.php' ),
+    )
+);
+// *************************************************************************************
+// Test Folder
+// *************************************************************************************
+$test_folder = array( 'text' => i18n('Test', 'r'), 'cls' => 'folder', 'expanded' => true, 'children' =>
+    array(
+    )
+);
+// *************************************************************************************
+// Here we can parse and edit folders keys values, before pushing them into $nav
+// ei. $admin_folder['expanded'] = false;
+// *************************************************************************************
+
+// $patient_folder['expanded']  = $_SESSION['nav']['patient_folder']['expanded'];
+// $fees_folder['expanded']     = $_SESSION['nav']['fees_folder']['expanded'];
+// $admin_folder['expanded']    = $_SESSION['nav']['admin_folder']['expanded'];
+// $misc_folder['expanded']     = $_SESSION['nav']['misc_folder']['expanded'];
+
+// *************************************************************************************
+// Lets push all the folders into $nav and print json return
+// *************************************************************************************
+array_push( $nav, $patient_folder, $fees_folder, $admin_folder, $misc_folder, $test_folder );
 print_r(json_encode($nav));
 ?>
