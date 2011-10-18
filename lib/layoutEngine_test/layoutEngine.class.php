362fdd45d776a19b776cb12711113d5ba21450f3
vela1606 <vela1606@gmail.com>
2011-10-18 10:37:30 -0400

    option ints and bools fixed

--- lib/layoutEngine_test/layoutEngine.class.php
+++ lib/layoutEngine_test/layoutEngine.class.php
@@ -22,70 +22,67 @@ class layoutEngineTest extends dbHelper {
     private $items = array();
 
     function getFileds($formPanel = null){
-        if ($formPanel != null){
-            // *********************************************************************************************************
-            // Get the Form ID from Database
-            // *********************************************************************************************************
-            $this->setSQL("Select * FROM forms_layout WHERE name = '$formPanel'");
-            $foo = $this->fetch(PDO::FETCH_ASSOC);
-            $fid = $foo['id'];
-            // *********************************************************************************************************
-            // Get Parent Items
-            // *********************************************************************************************************
-            $this->setSQL("Select * FROM forms_fields WHERE form_id = '$fid'");
-            foreach($this->execStatement(PDO::FETCH_ASSOC) as $item){
-                // *****************************************************************************************************
-                // Get Option for main parent item
-                // *****************************************************************************************************
-                $opts = $this->getItmesOptions($item['id']);
-                foreach($opts as $opt => $val){
-                    $item[$opt] = $val;
-                }
-                // *****************************************************************************************************
-                // Get Child Items
-                // *****************************************************************************************************
-                $item['items'] = $this->getChildItems($item['id']);
-                if($item['items'] == null) unset($item['items']);
-                // *****************************************************************************************************
-                // Delete unused by Sencha
-                // *****************************************************************************************************
-                unset($item['id']);
-                unset($item['form_id']);
-                unset($item['item_of']);
-                // Push the Items into Items Array!
-                array_push($this->items,$item);
-            }
-            // echo '<pre>';
-            // print_r($this->items);
-            // *********************************************************************************************************
-            // Lets use json_decode to return all the fields
-            // *********************************************************************************************************
-            return json_encode($this->items);
-        }
-    }
-
-    function getChildItems($parent){
-        // *************************************************************************************************************
-        // Reclusive function to get all child items
-        // *************************************************************************************************************
-        $items = array();
-        $this->setSQL("Select * FROM forms_fields WHERE item_of = '$parent'");
+        // *********************************************************************************************************
+        // Get the Form ID from Database
+        // *********************************************************************************************************
+        $this->setSQL("Select * FROM forms_layout WHERE name = '$formPanel'");
+        $foo = $this->fetch(PDO::FETCH_ASSOC);
+        $fid = $foo['id'];
+        // *********************************************************************************************************
+        // Get Parent Items
+        // *********************************************************************************************************
+        $this->setSQL("Select * FROM forms_fields WHERE form_id = '$fid'");
         foreach($this->execStatement(PDO::FETCH_ASSOC) as $item){
-            // *********************************************************************************************************
-            // Get option for Item
-            // *********************************************************************************************************
+            // *****************************************************************************************************
+            // Get Option for main parent item
+            // *****************************************************************************************************
             $opts = $this->getItmesOptions($item['id']);
             foreach($opts as $opt => $val){
                 $item[$opt] = $val;
             }
-            // *********************************************************************************************************
-            // Lets run this function again. This is wah makes this function reclusive
-            // *********************************************************************************************************
+            // *****************************************************************************************************
+            // Get Child Items
+            // *****************************************************************************************************
             $item['items'] = $this->getChildItems($item['id']);
             if($item['items'] == null) unset($item['items']);
-            // *********************************************************************************************************
+            // *****************************************************************************************************
             // Delete unused by Sencha
-            // *********************************************************************************************************
+            // *****************************************************************************************************
+            unset($item['id']);
+            unset($item['form_id']);
+            unset($item['item_of']);
+            // Push the Items into Items Array!
+            array_push($this->items,$item);
+        }
+        // echo '<pre>';
+        // print_r($this->items);
+        // *********************************************************************************************************
+        // Lets use json_decode to return all the fields
+        // *********************************************************************************************************
+        return json_encode($this->items);
+    }
+    // *************************************************************************************************************
+    // Reclusive function to get all child items
+    // *************************************************************************************************************
+    function getChildItems($parent){
+        $items = array();
+        $this->setSQL("Select * FROM forms_fields WHERE item_of = '$parent'");
+        foreach($this->execStatement(PDO::FETCH_ASSOC) as $item){
+            // *****************************************************************************************************
+            // Get option for Item
+            // *****************************************************************************************************
+            $opts = $this->getItmesOptions($item['id']);
+            foreach($opts as $opt => $val){
+                $item[$opt] = $val;
+            }
+            // *****************************************************************************************************
+            // Lets run this function again. This is what makes this function reclusive
+            // *****************************************************************************************************
+            $item['items'] = $this->getChildItems($item['id']);
+            if($item['items'] == null) unset($item['items']);
+            // *****************************************************************************************************
+            // Delete unused by Sencha
+            // *****************************************************************************************************
             unset($item['id']);
             unset($item['form_id']);
             unset($item['item_of']);
@@ -94,11 +91,22 @@ class layoutEngineTest extends dbHelper {
         }
         return $items;
     }
-
+    // *************************************************************************************************************
+    // Function to get items options/params
+    // *************************************************************************************************************
     function getItmesOptions($item_id){
-
+        $foo = array();
         $this->setSQL("Select * FROM forms_field_options WHERE field_id = '$item_id'");
         foreach($this->execStatement(PDO::FETCH_ASSOC) as $option){
+            
+            if(is_numeric($option['ovalue'])){      // if the string is numeric intval() the value to remove the comas
+                $option['ovalue'] = intval($option['ovalue']);
+            }elseif($option['ovalue'] == 'true'){   // if the sring is true let change the value to a bool
+                $option['ovalue'] = true;
+            }elseif($option['ovalue'] == 'false'){  // if the sring is false let change the value to a bool
+                $option['ovalue'] = false;
+            }
+            
             $foo[$option['oname']] = $option['ovalue'];
         }
         return $foo;
@@ -106,16 +114,15 @@ class layoutEngineTest extends dbHelper {
 
 	//**********************************************************************
 	// renderForm 
-	//
-	// This will render the selected form, and returns the Sencha ExtJS v4 
+	// This will render the selected form, and returns the Sencha ExtJS v4
 	// code.
 	//**********************************************************************
 	public function renderForm($formPanel, $path, $title, $labelWidth, $saveText){
-		$bBuff = "";
-		$bBuff .= "panel." . $formPanel . " = Ext.create('Ext.form.Panel', {";
-		$bBuff .= "title: '" . $title . "',";
+	    $bBuff = "";
+	    $bBuff .= "panel." . $formPanel . " = Ext.create('Ext.form.Panel', {";
+	    $bBuff .= "title: '" . $title . "',";
 		$bBuff .= "frame: true, bodyStyle: 'padding: 5px', layout: 'anchor', height:300,";
-		$bBuff .= "fieldDefaults: {labelAlign: 'top', msgTarget: 'side', anchor: '40%'},";
+		$bBuff .= "fieldDefaults: { msgTarget: 'side' },";
 		$bBuff .= "items:";
         $bBuff .= $this->getFileds($formPanel);
 		$bBuff .= "";
@@ -123,6 +130,7 @@ class layoutEngineTest extends dbHelper {
 		print $bBuff;
 	}
 }
+
 //$classname = new layoutEngineTest();
 //$classname->renderForm("Demographics", "app/patient_file/new", "New Patient", 300, "Save new patient");
 
