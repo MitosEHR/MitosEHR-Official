6cc2cfe600a692173bcb3ce38330776a9a5973b7
vela1606 <vela1606@gmail.com>
2011-10-31 21:43:48 -0400

    Ext.mitos.form.FormPanel class added

---
+++ classes/ext/mitos/form/FormPanel.js
@@ -0,0 +1,14 @@
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Ernesto J. Rodriguez (Certun)
+ * File:
+ * Date: 10/31/11
+ * Time: 3:24 PM
+ */
+Ext.define('Ext.mitos.form.FormPanel',{
+	extend      : 'Ext.form.Panel',
+    alias       : 'widget.mitosform',
+    bodyStyle   : 'padding: 5px;',
+	autoWidth   : true,
+	border      : false
+});
