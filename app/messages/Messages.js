28959af0b56de49b868ec6e0d7a0cf1a67d75b8b
vela1606 <vela1606@gmail.com>
2011-11-29 14:08:19 -0500

    re designing messages...

--- app/messages/Messages.js
+++ app/messages/Messages.js
@@ -2,6 +2,8 @@ Ext.define('Ext.mitos.panel.messages.Messages',{
     extend      : 'Ext.mitos.RenderPanel',
     id          : 'panelMessages',
     pageTitle   : 'Messages',
+    pageLayout  : 'border',
+    defaults:{split: true},
     uses        : [
         'Ext.mitos.restStoreModel',
         'Ext.mitos.GridPanel',
@@ -13,9 +15,9 @@ Ext.define('Ext.mitos.panel.messages.Messages',{
     ],
     initComponent: function(){
         var me = this;
-        // *************************************************************************************
-        // Message Store
-        // *************************************************************************************
+        /**
+         * Message Store
+         */
         me.storeMsgs = Ext.create('Ext.mitos.restStoreModel',{
             fields: [
                 {name: 'id',				type: 'int'},
@@ -40,28 +42,27 @@ Ext.define('Ext.mitos.panel.messages.Messages',{
             url		    : 'app/messages/data.php'
         });
 
-        // *************************************************************************************
-        // Message GridPanel
-        // *************************************************************************************
-        me.msgGrid = Ext.create('Ext.grid.Panel', {
+        /**
+         * Message GridPanel
+         */
+        me.msgGrid = Ext.create('Ext.mitos.GridPanel', {
             store		: me.storeMsgs,
-            autoHeight 	: true,
-            border     	: true,
-            frame		: true,
-            loadMask    : true,
+            region      : 'center',
+            border:true,
             viewConfig 	: {forceFit: true, stripeRows : true},
             listeners: {
                 scope       : this,
+                itemclick   : this.onItemClick,
                 itemdblclick: this.onItemdblclick
             },
             columns: [
-                { header: 'noteid',     sortable: false, dataIndex: 'noteid',           hidden: true},
-                { header: 'reply_id',   sortable: false, dataIndex: 'reply_id',         hidden: true},
-                { header: 'user',       sortable: false, dataIndex: 'user',             hidden: true},
-                { header: 'Status',     sortable: true,  dataIndex: 'message_status',   width : 70 },
-                { header: 'From',       sortable: true,  dataIndex: 'user',             width : 200 },
-                { header: 'Patient',    sortable: true,  dataIndex: 'patient_name',     width : 200 },
-                { header: 'Subject',    sortable: true,  dataIndex: 'subject',          flex  : 1 },
+                { header: 'noteid',     sortable: false, dataIndex: 'noteid',           hidden: true },
+                { header: 'reply_id',   sortable: false, dataIndex: 'reply_id',         hidden: true },
+                { header: 'user',       sortable: false, dataIndex: 'user',             hidden: true },
+                { header: 'Status',     sortable: true,  dataIndex: 'message_status',   width : 70   },
+                { header: 'From',       sortable: true,  dataIndex: 'user',             width : 200  },
+                { header: 'Patient',    sortable: true,  dataIndex: 'patient_name',     width : 200  },
+                { header: 'Subject',    sortable: true,  dataIndex: 'subject',          flex  : 1    },
                 { header: 'Type',       sortable: true,  dataIndex: 'note_type',        width : 100  }
 
             ],
@@ -69,14 +70,62 @@ Ext.define('Ext.mitos.panel.messages.Messages',{
                 text	: 'New message',
                 iconCls	: 'newMessage',
                 itemId  : 'newMsg',
-                handler: function(){
+                handler : function(){
                     me.onNew();
                 }
             }]
-        }); // End Message GridPanel
-        // *************************************************************************************
-        // Message Window
-        // *************************************************************************************
+        });
+        /**
+         *  Message Preview and Form panels
+         */
+        me.msgContainer = Ext.create('Ext.panel.Panel',{
+            region  : 'south',
+            //layout  : 'fit',
+            split   : true,
+            border  : true,
+            height  : 300,
+            tbar:[{
+                text:'Reply'
+            },{
+                text:'Foward'
+            },{
+                text:'Delete',
+                cls:'toolDelete'
+            }]
+        });
+        /**
+         * default item form msgContainer (this is the "no message selected" text)
+         * we are using css background style to display a pretty background image
+         * insted of using plain text
+         */
+        me.msgContainer.add({border:false,html:'<div class="noMsg"></div>'});
+        /**
+         * message preview panel using a custom template
+         */
+        me.msgPreView = Ext.create('Ext.panel.Panel',{
+            border: false,
+            tpl : Ext.create('Ext.XTemplate',
+                '<div class="message-view-container">'+
+                    '<div class="message-view-header">'+
+                        '<div class="message-view-header-left">'+
+                            '<p><span>From:</span> {user}</p>'+
+                            '<p><span>Patient:</span> {patient_name}</p>'+
+                            '<p><span>Subject:</span> {subject}</p>'+
+                        '</div>'+
+                        '<div class="message-view-header-right">'+
+                            '<p><span>Type:</span> {note_type}</p>'+
+                            '<p><span>Status:</span> {message_status}</p>'+
+                        '</div>'+
+                    '</div>'+
+                    '<div class="message-view-body">{body}</div>'+
+                '</dic>'
+            )
+
+        });
+        /**
+         * to be replace....
+         * trying to remove all the windows and use panels to handle this
+         */
         me.winMessage = Ext.create('Ext.mitos.window.Window', {
             width		: 700,
             title		: 'Compose Message',
@@ -114,13 +163,13 @@ Ext.define('Ext.mitos.panel.messages.Messages',{
                     fieldLabel  : 'Subject',
                     name        : 'subject'
                 },{
-                    xtype       : 'textarea',
+                    xtype       : 'htmleditor',
                     readOnly    : true,
                     name        : 'body',
                     itemId      : 'bodyMsg',
                     height      : 100
                 },{
-                    xtype       : 'textarea',
+                    xtype       : 'htmleditor',
                     name        : 'curr_msg',
                     itemId      : 'currMsg',
                     allowBlank  : false,
@@ -180,9 +229,14 @@ Ext.define('Ext.mitos.panel.messages.Messages',{
                 }
             }
         });
-        me.pageBody = [ me.msgGrid ];
+        me.pageBody = [ me.msgGrid, me.msgContainer ];
         me.callParent(arguments);
     }, // End initComponent
+    /**
+     * onNew will reset the form and load a new model
+     * with message_status value set to New, and
+     * note_type value set to Unassigned
+     */
     onNew:function(){
         var form = this.winMessage.down('form');
         form.getForm().reset();
@@ -194,14 +248,17 @@ Ext.define('Ext.mitos.panel.messages.Messages',{
         form.getForm().loadRecord(newModel);
         this.action('new');
     },
+    /**
+     *
+     * @param form
+     * @param store
+     */
     onSave:function(form, store){
         var record      = form.getRecord(),
             values      = form.getValues(),
             storeIndex  = store.indexOf(record);
-
         if (storeIndex == -1){
             store.add(values);
-
         }else{
             record.set(values);
         }
@@ -209,11 +266,16 @@ Ext.define('Ext.mitos.panel.messages.Messages',{
         store.load();
         this.winMessage.close();
     },
+    /**
+     *
+     * @param form
+     * @param store
+     */
     onDelete:function(form, store){
         Ext.Msg.show({
             title   : 'Please confirm...',
             icon    : Ext.MessageBox.QUESTION,
-            msg     :'Are you sure to delete this message?',
+            msg     : 'Are you sure to delete this message?',
             buttons : Ext.Msg.YESNO,
             scope   : this,
             fn :function(btn){
@@ -221,32 +283,67 @@ Ext.define('Ext.mitos.panel.messages.Messages',{
                     var currentRec = form.getRecord();
                     store.remove(currentRec);
                     store.destroy();
+                    this.msgPreView.body.update('<div class="noMsg"></div>');
                     this.winMessage.close();
                 }
             }
         });
     },
+    /**
+     * On item click check if msgPreView is already inside the container.
+     * if not, remove the item inside the container, add msgPreView and update it with record data.
+     * if yes, just update the msgPreView with the new record data
+     *
+     * @param view
+     * @param record
+     */
+    onItemClick: function(view, record){
+
+        if(this.msgContainer.items.items[0] != this.msgPreView ){
+            this.msgContainer.remove(0);
+            this.msgContainer.add(this.msgPreView);
+        }
+        this.msgPreView.update(record.data);
+    },
+    /**
+     * On item double clik load the new record to the form
+     * and call action(old)
+     *
+     * @param view
+     * @param record
+     */
     onItemdblclick: function(view, record) {
         var form = this.winMessage.down('form');
         form.getForm().loadRecord(record);
-        this.action('itemdblclick');
+        this.action('old');
     },
+    /**
+     * This function its called from the the MitosApp.js 
+     */
     loadStores:function(){
         this.storeMsgs.load();
     },
+    /**
+     * This function is use to disable/enabled and hide/show buttons and fields
+     * according to the action
+     *
+     * @param action
+     */
     action:function(action) {
-        var win = this.winMessage,
-            sm = this.msgGrid.getSelectionModel(),
-            gridTbar = this.msgGrid.down('toolbar'),
-            winTbar = win.down('toolbar'),
-            form = win.down('form'),
+        var win          = this.winMessage,
+            sm           = this.msgGrid.getSelectionModel(),
+            gridTbar     = this.msgGrid.down('toolbar'),
+            //msgPreView   = this.msgPreView,
+            //msgContainer = this.msgContainer,
+            winTbar      = win.down('toolbar'),
+            form         = win.down('form'),
             patientCombo = form.getComponent('patientCombo'),
             patientField = form.getComponent('patientField'),
-            bodyMsg = form.getComponent('bodyMsg'),
-            currMsg = form.getComponent('currMsg'),
-            newbtn = gridTbar.getComponent('newMsg'),
-            deletebtn = winTbar.getComponent('deleteMsg'),
-            replybtn = winTbar.getComponent('replyMsg');
+            bodyMsg      = form.getComponent('bodyMsg'),
+            currMsg      = form.getComponent('currMsg'),
+            newbtn       = gridTbar.getComponent('newMsg'),
+            deletebtn    = winTbar.getComponent('deleteMsg'),
+            replybtn     = winTbar.getComponent('replyMsg');
         if (action == 'new') {
             bodyMsg.hide();
             currMsg.show();
@@ -254,6 +351,15 @@ Ext.define('Ext.mitos.panel.messages.Messages',{
             patientCombo.show();
             patientField.hide();
             win.show();
+        } else if (action == 'old') {
+            replybtn.enable();
+            bodyMsg.show();
+            currMsg.hide();
+            patientCombo.hide();
+            patientField.show();
+            deletebtn.enable();
+            win.show();
+            bodyMsg.getToolbar().hide();
         } else if (action == 'reply') {
             currMsg.show();
             patientCombo.hide();
@@ -264,14 +370,10 @@ Ext.define('Ext.mitos.panel.messages.Messages',{
             newbtn.enable();
             deletebtn.disable();
             sm.deselectAll();
-        } else if (action == 'itemdblclick') {
-            replybtn.enable();
-            bodyMsg.show();
-            currMsg.hide();
-            patientCombo.hide();
-            patientField.show();
-            deletebtn.enable();
-            win.show();
+        } else if (action == 'itemclick') {
+
+
+
         }
     }
-}); //end Ext.mitos.panel.messages.Messages
+});
