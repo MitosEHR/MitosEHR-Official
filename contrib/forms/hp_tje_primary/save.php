f8a5b7da8526000a8b856d1001e2eca5c92db4db
Ernesto J Rodriguez <vela1606@gmail.com>
2012-02-01 11:23:19 -0400

    contrib dir removed

--- contrib/forms/hp_tje_primary/save.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once("../../registry.php");
-include_once("$srcdir/acl.inc.php");
-
-require ("C_FormHpTje.class.php");
-$c = new C_FormHpTje();
-echo $c->default_action_process($_POST);
-@formJump();
-?>
--- contrib/forms/prosthesis/new.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once("../../registry.php");
-include_once("$srcdir/acl.inc.php");
-
-require ("C_FormProsthesis.class.php");
-
-$c = new C_FormProsthesis();
-echo $c->default_action();
-?>
