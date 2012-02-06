674b75d06e42e20453c68f1e547e1ac5504586f9
Ernesto J Rodriguez <vela1606@gmail.com>
2012-02-05 23:15:26 -0400

    Ext.drive progress

--- classes/Logs.php
+++ classes/Logs.php
@@ -1,19 +1,44 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Ernesto J. Rodriguez (Certun)
- * File: Logs.php
- * Date: 2/4/12
- * Time: 12:27 AM
- */
 if(!isset($_SESSION)){
     session_name ("MitosEHR" );
     session_start();
     session_cache_limiter('private');
 }
 include_once('dbhelper.php');
+/**
+ * This Class is used to handle all the log requests
+ *
+ * Created by JetBrains PhpStorm.
+ * User: Ernesto J. Rodriguez (Certun)
+ * File: Logs.php
+ * Date: 2/4/12
+ * Time: 12:27 AM
+ *
+ * @package default
+ */
 class Logs extends dbHelper {
 
+    /**
+     * Get Logs
+     *
+     * This will return the total recods and the filtered
+     * record by Start and Limit params.
+     *
+     * <code>
+     * public function getLogs(stdClass $params){
+     *
+     *        $this->setSQL("SELECT * FROM log ORDER BY id DESC");
+     *        $rows   = $this->execStatement(PDO::FETCH_CLASS);
+     *        $total  = count($rows);
+     *        $rows = $this->filertByStartLimit($rows,$params);
+     *        return array('totals'=>$total ,'rows'=>$rows);
+     *
+     *    }
+     *</code>
+     *
+     * @param stdClass $params
+     * @return array
+     */
     public function getLogs(stdClass $params){
 
         $this->setSQL("SELECT * FROM log ORDER BY id DESC");
