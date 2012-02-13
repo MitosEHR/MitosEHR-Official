1930147d195ee98b1bed1d3e98cb5988dce06739
Ernesto J Rodriguez <vela1606@gmail.com>
2012-02-13 12:36:44 -0400

    Directories changed to resemble a vmc architecture.

--- app/index.php
+++ app/index.php
@@ -42,7 +42,7 @@ Globals::setGlobals();
 <link rel="stylesheet" type="text/css" href="ui_app/style_newui.css" >
 <link rel="stylesheet" type="text/css" href="ui_app/mitosehr_app.css" >
 <link rel="shortcut icon" href="favicon.ico" >
-<!--<script type="text/javascript" src="app/App.js"></script>-->
+<!--<script type="text/javascript" src="app/view/App.js"></script>-->
 </head>
     <body>
         <div id="mainapp-loading-mask" class="x-mask mitos-mask"></div>
@@ -71,9 +71,8 @@ Globals::setGlobals();
                 enabled			: true,
                 disableCaching	: false,
                 paths			: {
-                    'Ext.ux'            : '<?php echo $_SESSION["dir"]["ext_classes"]; ?>/ux',
-                    'Ext.mitos'         : '<?php echo $_SESSION["dir"]["ext_classes"]; ?>',
-                    'Ext.mitos.panel'   : 'app',
+                    'Ext.ux'            : 'app/classes/ux',
+                    'Ext.mitos'         : 'app',
                     'Extensible'        : 'lib/extensible-1.5.0/src'
                 }
             });
@@ -82,75 +81,75 @@ Globals::setGlobals();
         <script src="data/api.php"></script>
 
         <script type="text/javascript" src="lib/ext-4.0.7/src/grid/plugin/RowEditing.js"></script>
-        <script type="text/javascript" src="classes/ext/Overrides.js"></script>
+        <script type="text/javascript" src="app/classes/Overrides.js"></script>
 
 
 
-        <script type="text/javascript" src="classes/ext/NodeDisabled.js"></script>
+        <script type="text/javascript" src="app/classes/NodeDisabled.js"></script>
 
-        <script type="text/javascript" src="classes/ext/RenderPanel.js"></script>
+        <script type="text/javascript" src="app/classes/RenderPanel.js"></script>
 
-        <script type="text/javascript" src="app/dashboard/Dashboard.js"></script>
+        <script type="text/javascript" src="app/view/dashboard/Dashboard.js"></script>
         <script type="text/javascript" src="lib/extensible-1.5.0/src/calendar/data/EventStore.js"></script>
-        <script type="text/javascript" src="classes/ext/ux/SlidingPager.js"></script>
+        <script type="text/javascript" src="app/classes/ux/SlidingPager.js"></script>
 
 
-        <script type="text/javascript" src="classes/ext/GridPanel.js"></script>
-        <script type="text/javascript" src="classes/ext/LivePatientSearch.js"></script>
-        <script type="text/javascript" src="classes/ext/ManagedIframe.js"></script>
-        <script type="text/javascript" src="classes/ext/PhotoIdWindow.js"></script>
-        <script type="text/javascript" src="classes/ext/restStoreModel.js"></script>
-        <script type="text/javascript" src="classes/ext/CalCategoryComboBox.js"></script>
-        <script type="text/javascript" src="classes/ext/CalStatusComboBox.js"></script>
+        <script type="text/javascript" src="app/classes/GridPanel.js"></script>
+        <script type="text/javascript" src="app/classes/LivePatientSearch.js"></script>
+        <script type="text/javascript" src="app/classes/ManagedIframe.js"></script>
+        <script type="text/javascript" src="app/classes/PhotoIdWindow.js"></script>
+        <script type="text/javascript" src="app/classes/restStoreModel.js"></script>
+        <script type="text/javascript" src="app/classes/CalCategoryComboBox.js"></script>
+        <script type="text/javascript" src="app/classes/CalStatusComboBox.js"></script>
 
-        <script type="text/javascript" src="classes/ext/combo/Authorizations.js"></script>
-        <script type="text/javascript" src="classes/ext/combo/CodesTypes.js"></script>
-        <script type="text/javascript" src="classes/ext/combo/Facilities.js"></script>
-        <script type="text/javascript" src="classes/ext/combo/InsurancePayerType.js"></script>
-        <script type="text/javascript" src="classes/ext/combo/Languages.js"></script>
-        <script type="text/javascript" src="classes/ext/combo/Lists.js"></script>
-        <script type="text/javascript" src="classes/ext/combo/MsgNoteType.js"></script>
-        <script type="text/javascript" src="classes/ext/combo/MsgStatus.js"></script>
-        <script type="text/javascript" src="classes/ext/combo/posCodes.js"></script>
-        <script type="text/javascript" src="classes/ext/combo/Roles.js"></script>
-        <script type="text/javascript" src="classes/ext/combo/TaxId.js"></script>
-        <script type="text/javascript" src="classes/ext/combo/Titles.js"></script>
-        <script type="text/javascript" src="classes/ext/combo/TransmitMedthod.js"></script>
-        <script type="text/javascript" src="classes/ext/combo/Types.js"></script>
-        <script type="text/javascript" src="classes/ext/combo/Users.js"></script>
-
-
-        <script type="text/javascript" src="classes/ext/form/fields/Checkbox.js"></script>
-        <script type="text/javascript" src="classes/ext/form/fields/DateTime.js"></script>
+        <script type="text/javascript" src="app/classes/combo/Authorizations.js"></script>
+        <script type="text/javascript" src="app/classes/combo/CodesTypes.js"></script>
+        <script type="text/javascript" src="app/classes/combo/Facilities.js"></script>
+        <script type="text/javascript" src="app/classes/combo/InsurancePayerType.js"></script>
+        <script type="text/javascript" src="app/classes/combo/Languages.js"></script>
+        <script type="text/javascript" src="app/classes/combo/Lists.js"></script>
+        <script type="text/javascript" src="app/classes/combo/MsgNoteType.js"></script>
+        <script type="text/javascript" src="app/classes/combo/MsgStatus.js"></script>
+        <script type="text/javascript" src="app/classes/combo/posCodes.js"></script>
+        <script type="text/javascript" src="app/classes/combo/Roles.js"></script>
+        <script type="text/javascript" src="app/classes/combo/TaxId.js"></script>
+        <script type="text/javascript" src="app/classes/combo/Titles.js"></script>
+        <script type="text/javascript" src="app/classes/combo/TransmitMedthod.js"></script>
+        <script type="text/javascript" src="app/classes/combo/Types.js"></script>
+        <script type="text/javascript" src="app/classes/combo/Users.js"></script>
 
 
-        <script type="text/javascript" src="app/patientfile/visits/Visits.js"></script>
-        <script type="text/javascript" src="app/patientfile/summary/Summary.js"></script>
-        <script type="text/javascript" src="app/patientfile/encounter/Encounter.js"></script>
-        <script type="text/javascript" src="app/patientfile/new/NewPatient.js"></script>
+        <script type="text/javascript" src="app/classes/form/fields/Checkbox.js"></script>
+        <script type="text/javascript" src="app/classes/form/fields/DateTime.js"></script>
 
 
-        <script type="text/javascript" src="app/administration/Facilities.js"></script>
-        <script type="text/javascript" src="app/administration/Globals.js"></script>
-        <script type="text/javascript" src="app/administration/Layout.js"></script>
-        <script type="text/javascript" src="app/administration/Lists.js"></script>
-        <script type="text/javascript" src="app/administration/Log.js"></script>
-        <script type="text/javascript" src="app/administration/Practice.js"></script>
-        <script type="text/javascript" src="app/administration/Roles.js"></script>
-        <script type="text/javascript" src="app/administration/Services.js"></script>
-        <script type="text/javascript" src="app/administration/Users.js"></script>
-
-        <script type="text/javascript" src="app/messages/Messages.js"></script>
-        <script type="text/javascript" src="app/search/PatientSearch.js"></script>
-
-        <script type="text/javascript" src="app/miscellaneous/addressbook/Addressbook.js"></script>
-        <script type="text/javascript" src="app/miscellaneous/myaccount/MyAccount.js"></script>
-        <script type="text/javascript" src="app/miscellaneous/mysettings/MySettings.js"></script>
-        <script type="text/javascript" src="app/miscellaneous/officenotes/OfficeNotes.js"></script>
-        <script type="text/javascript" src="app/miscellaneous/websearch/Websearch.js"></script>
+        <script type="text/javascript" src="app/view/patientfile/visits/Visits.js"></script>
+        <script type="text/javascript" src="app/view/patientfile/summary/Summary.js"></script>
+        <script type="text/javascript" src="app/view/patientfile/encounter/Encounter.js"></script>
+        <script type="text/javascript" src="app/view/patientfile/new/NewPatient.js"></script>
 
 
-        <script type="text/javascript" src="app/MitosApp.js"></script>
+        <script type="text/javascript" src="app/view/administration/Facilities.js"></script>
+        <script type="text/javascript" src="app/view/administration/Globals.js"></script>
+        <script type="text/javascript" src="app/view/administration/Layout.js"></script>
+        <script type="text/javascript" src="app/view/administration/Lists.js"></script>
+        <script type="text/javascript" src="app/view/administration/Log.js"></script>
+        <script type="text/javascript" src="app/view/administration/Practice.js"></script>
+        <script type="text/javascript" src="app/view/administration/Roles.js"></script>
+        <script type="text/javascript" src="app/view/administration/Services.js"></script>
+        <script type="text/javascript" src="app/view/administration/Users.js"></script>
+
+        <script type="text/javascript" src="app/view/messages/Messages.js"></script>
+        <script type="text/javascript" src="app/view/search/PatientSearch.js"></script>
+
+        <script type="text/javascript" src="app/view/miscellaneous/addressbook/Addressbook.js"></script>
+        <script type="text/javascript" src="app/view/miscellaneous/myaccount/MyAccount.js"></script>
+        <script type="text/javascript" src="app/view/miscellaneous/mysettings/MySettings.js"></script>
+        <script type="text/javascript" src="app/view/miscellaneous/officenotes/OfficeNotes.js"></script>
+        <script type="text/javascript" src="app/view/miscellaneous/websearch/Websearch.js"></script>
+
+
+        <script type="text/javascript" src="app/view/MitosApp.js"></script>
 
 
 
@@ -161,11 +160,10 @@ Globals::setGlobals();
             }
 
 
-
             Ext.direct.Manager.addProvider(Ext.mitos.data.REMOTING_API);
 
             Ext.onReady(function(){
-                App = Ext.create('Ext.mitos.panel.MitosApp');
+                App = Ext.create('Ext.mitos.view.MitosApp');
             });
         </script>
 
