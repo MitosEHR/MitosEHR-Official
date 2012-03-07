6ccba8929ccd5d00f03059bf148828e41ce39631
Ernesto J Rodriguez <vela1606@gmail.com>
2012-03-07 00:33:15 -0400

    encounter progress note

--- classes/Encounter.php
+++ classes/Encounter.php
@@ -30,7 +30,8 @@ class Encounter {
      */
     private $patient;
 
-    function __construct(){
+    function __construct()
+    {
         $this->db = new dbHelper();
         $this->user = new User();
         $this->patient = new Patient();
@@ -39,7 +40,8 @@ class Encounter {
     /**
      * @return array
      */
-    public function ckOpenEncounters(){
+    public function ckOpenEncounters()
+    {
         $pid =  $_SESSION['patient']['pid'];
         $this->db->setSQL("SELECT * FROM form_data_encounter WHERE pid = '$pid' AND close_date IS NULL");
         $total = $this->db->rowCount();
@@ -54,7 +56,8 @@ class Encounter {
      * @param stdClass $params
      * @return array
      */
-    public function getEncounters(stdClass $params){
+    public function getEncounters(stdClass $params)
+    {
 
         if(isset($params->sort)){
             $ORDER = 'ORDER BY ' . $params->sort[0]->property . ' ' . $params->sort[0]->direction;
@@ -78,7 +81,8 @@ class Encounter {
      * @param stdClass $params
      * @return array
      */
-    public function createEncounter(stdClass $params){
+    public function createEncounter(stdClass $params)
+    {
 
         $params->pid        = $_SESSION['patient']['pid'];
         $params->open_uid   = $_SESSION['user']['id'];
@@ -117,7 +121,8 @@ class Encounter {
      * @param stdClass $params
      * @return array|mixed
      */
-    public function getEncounter(stdClass $params){
+    public function getEncounter(stdClass $params)
+    {
         $this->db->setSQL("SELECT * FROM form_data_encounter WHERE eid = '$params->eid'");
         $encounter = $this->db->fetch(PDO::FETCH_ASSOC);
 
@@ -138,119 +143,10 @@ class Encounter {
      * @param stdClass $params
      * @return array|mixed
      */
-    public function updateEncounter(stdClass $params){
+    public function updateEncounter(stdClass $params)
+
+    {
         return array("success" => true, 'encounter' => $params);
-    }
-
-    public function getVitalsByPid($pid){
-        $this->db->setSQL("SELECT * FROM form_data_vitals WHERE pid = '$pid' ORDER BY date DESC");
-        $rows = array();
-        foreach($this->db->execStatement(PDO::FETCH_ASSOC) as $row){
-            $row['height_in'] = intval($row['height_in']);
-            $row['height_cn'] = intval($row['height_cn']);
-            $row['administer'] = $this->user->getUserNameById($row['uid']);
-            array_push($rows, $row);
-        }
-        return $rows;
-    }
-
-    public function getVitalsByEid($eid){
-        $this->db->setSQL("SELECT * FROM form_data_vitals WHERE eid = '$eid' ORDER BY date DESC");
-        $rows = array();
-        foreach($this->db->execStatement(PDO::FETCH_ASSOC) as $row){
-            $row['height_in'] = intval($row['height_in']);
-            $row['height_cn'] = intval($row['height_cn']);
-            $row['administer'] = $this->user->getUserNameById($row['uid']);
-            array_push($rows, $row);
-        }
-        return $rows;
-    }
-
-    /**
-     * @param stdClass $params
-     * @return array
-     */
-    public function getVitals(stdClass $params){
-
-        $pid =  (isset($params->pid)) ? $params->pid : $_SESSION['patient']['pid'];
-
-        $this->db->setSQL("SELECT * FROM form_data_vitals WHERE pid = '$pid' ORDER BY date DESC");
-        $total = $this->db->rowCount();
-        $rows = array();
-        foreach($this->db->execStatement(PDO::FETCH_ASSOC) as $row){
-            $row['height_in'] = intval($row['height_in']);
-            $row['height_cn'] = intval($row['height_cn']);
-            $row['administer'] = $this->user->getUserNameById($row['uid']);
-            array_push($rows, $row);
-        }
-        if($total >= 1){
-            return $rows;
-        }else{
-            return array("success" => true);
-        }
-    }
-
-    public function addVitals(stdClass $params){
-        $data = get_object_vars($params);
-        unset($data['administer']);
-        $data['date'] = $this->parseDate($data['date']);
-        $sql = $this->db->sqlBind($data, 'form_data_vitals', 'I');
-        $this->db->setSQL($sql);
-        $this->db->execLog();
-        return $params;
-    }
-
-
-    public function getSoapByEid($eid){
-        $this->db->setSQL("SELECT * FROM form_data_soap WHERE eid = '$eid' ORDER BY date DESC");
-        return $this->db->execStatement(PDO::FETCH_ASSOC);
-    }
-
-    public function getReviewOfSystemsChecksByEid($eid){
-        $this->db->setSQL("SELECT * FROM form_data_review_of_systems_check WHERE eid = '$eid' ORDER BY date DESC");
-        return $this->db->execStatement(PDO::FETCH_ASSOC);
-    }
-    public function getReviewOfSystemsByEid($eid){
-        $this->db->setSQL("SELECT * FROM form_data_review_of_systems WHERE eid = '$eid' ORDER BY date DESC");
-        $record = $this->db->fetch();
-        foreach($record as $key => $val){
-            $record[$key] =  ($val == null)? 'null' : $val;
-        }
-        return $record;
-    }
-    public function getDictationByEid($eid){
-        $this->db->setSQL("SELECT * FROM form_data_dictation WHERE eid = '$eid' ORDER BY date DESC");
-        return $this->db->execStatement(PDO::FETCH_ASSOC);
-    }
-
-
-    public function updateSoapById(stdClass $params){
-        $data = get_object_vars($params);
-        unset($data['id']);
-        $this->db->setSQL($this->db->sqlBind($data, 'form_data_soap', 'U', "id='".$params->id."'"));
-        $this->db->execLog();
-        return $params;
-    }
-    public function updateReviewOfSystemsChecksById(stdClass $params){
-        $data = get_object_vars($params);
-        unset($data['id']);
-        $this->db->setSQL($this->db->sqlBind($data, 'form_data_review_of_systems_check', 'U', "id='".$params->id."'"));
-        $this->db->execLog();
-        return $params;
-    }
-    public function updateReviewOfSystemsById(stdClass $params){
-        $data = get_object_vars($params);
-        unset($data['id']);
-        $this->db->setSQL($this->db->sqlBind($data, 'form_data_review_of_systems', 'U', "id='".$params->id."'"));
-        $this->db->execLog();
-        return $params;
-    }
-    public function updateDictationById(stdClass $params){
-        $data = get_object_vars($params);
-        unset($data['id']);
-        $this->db->setSQL($this->db->sqlBind($data, 'form_data_dictation', 'U', "id='".$params->id."'"));
-        $this->db->execLog();
-        return $params;
     }
 
     /**
@@ -271,8 +167,264 @@ class Encounter {
             return array('success'=> false);
         }
     }
+    /**
+     * @param $pid
+     * @return array
+     */
+    public function getVitalsByPid($pid)
+    {
+        $this->db->setSQL("SELECT * FROM form_data_vitals WHERE pid = '$pid' ORDER BY date DESC");
+        $rows = array();
+        foreach($this->db->execStatement(PDO::FETCH_ASSOC) as $row){
+            $row['height_in'] = intval($row['height_in']);
+            $row['height_cn'] = intval($row['height_cn']);
+            $row['administer'] = $this->user->getUserNameById($row['uid']);
+            array_push($rows, $row);
+        }
+        return $rows;
+    }
+    /**
+     * @param $eid
+     * @return array
+     */
+    public function getVitalsByEid($eid)
+    {
+        $this->db->setSQL("SELECT * FROM form_data_vitals WHERE eid = '$eid' ORDER BY date DESC");
+        $rows = array();
+        foreach($this->db->execStatement(PDO::FETCH_ASSOC) as $row){
+            $row['height_in'] = intval($row['height_in']);
+            $row['height_cn'] = intval($row['height_cn']);
+            $row['administer'] = $this->user->getUserNameById($row['uid']);
+            array_push($rows, $row);
+        }
+        return $rows;
+    }
 
-    public function getProgressNoteByEid($eid){
+    /**
+     * @param stdClass $params
+     * @return array
+     */
+    public function getVitals(stdClass $params)
+    {
+        $pid =  (isset($params->pid)) ? $params->pid : $_SESSION['patient']['pid'];
+        $this->db->setSQL("SELECT * FROM form_data_vitals WHERE pid = '$pid' ORDER BY date DESC");
+        $total = $this->db->rowCount();
+        $rows = array();
+        foreach($this->db->execStatement(PDO::FETCH_ASSOC) as $row){
+            $row['height_in'] = intval($row['height_in']);
+            $row['height_cn'] = intval($row['height_cn']);
+            $row['administer'] = $this->user->getUserNameById($row['uid']);
+            array_push($rows, $row);
+        }
+        if($total >= 1){
+            return $rows;
+        }else{
+            return array("success" => true);
+        }
+    }
+    /**
+     * @param stdClass $params
+     * @return stdClass
+     */
+    public function addVitals(stdClass $params)
+    {
+        $data = get_object_vars($params);
+        unset($data['administer']);
+        $data['date'] = $this->parseDate($data['date']);
+        $sql = $this->db->sqlBind($data, 'form_data_vitals', 'I');
+        $this->db->setSQL($sql);
+        $this->db->execLog();
+        return $params;
+    }
+
+    //*************************************************************//
+    //******************** Medical Window *************************//
+    //************************ Stuff ******************************//
+    //*************************************************************//
+
+    /**
+     * @param $pid
+     * @return array
+     */
+    public function getImmunizationsByPid($pid)
+    {
+        $this->db->setSQL("SELECT * FROM patient_immunizations WHERE pid='$pid'");
+        return $this->db->execStatement(PDO::FETCH_ASSOC);
+    }
+    /**
+     * @param $eid
+     * @return array
+     */
+    public function getImmunizationsByEid($eid)
+    {
+        $this->db->setSQL("SELECT * FROM patient_immunizations WHERE eid='$eid'");
+        return $this->db->execStatement(PDO::FETCH_ASSOC);
+    }
+    /**
+     * @param $pid
+     * @return array
+     */
+    public function getAllergiesByPid($pid)
+    {
+        $this->db->setSQL("SELECT * FROM patient_allergies WHERE pid='$pid'");
+        return $this->db->execStatement(PDO::FETCH_ASSOC);
+    }
+    /**
+     * @param $eid
+     * @return array
+     */
+    public function getAllergiesByEid($eid)
+    {
+        $this->db->setSQL("SELECT * FROM patient_allergies WHERE eid='$eid'");
+        return $this->db->execStatement(PDO::FETCH_ASSOC);
+    }
+    /**
+     * @param $pid
+     * @return array
+     */
+    public function getMedicalIssuesByPid($pid)
+    {
+        $this->db->setSQL("SELECT * FROM patient_medical_issues WHERE pid='$pid'");
+        return $this->db->execStatement(PDO::FETCH_ASSOC);
+    }
+    /**
+     * @param $eid
+     * @return array
+     */
+    public function getMedicalIssuesByEid($eid)
+    {
+        $this->db->setSQL("SELECT * FROM patient_medical_issues WHERE eid='$eid'");
+        return $this->db->execStatement(PDO::FETCH_ASSOC);
+    }
+    /**
+     * @param $pid
+     * @return array
+     */
+    public function getSurgeriesByPid($pid)
+    {
+        $this->db->setSQL("SELECT * FROM patient_surgeries WHERE pid='$pid'");
+        return $this->db->execStatement(PDO::FETCH_ASSOC);
+    }
+    /**
+     * @param $eid
+     * @return array
+     */
+    public function getSurgeriesByEid($eid)
+    {
+        $this->db->setSQL("SELECT * FROM patient_surgeries WHERE eid='$eid'");
+        return $this->db->execStatement(PDO::FETCH_ASSOC);
+    }
+    /**
+     * @param $pid
+     * @return array
+     */
+    public function getDentalByPid($pid)
+    {
+        $this->db->setSQL("SELECT * FROM patient_dental WHERE pid='$pid'");
+        return $this->db->execStatement(PDO::FETCH_ASSOC);
+    }
+    /**
+     * @param $eid
+     * @return array
+     */
+    public function getDentalByEid($eid)
+    {
+        $this->db->setSQL("SELECT * FROM patient_dental WHERE eid='$eid'");
+        return $this->db->execStatement(PDO::FETCH_ASSOC);
+    }
+    /**
+     * @param $eid
+     * @return array
+     */
+    public function getSoapByEid($eid)
+    {
+        $this->db->setSQL("SELECT * FROM form_data_soap WHERE eid = '$eid' ORDER BY date DESC");
+        return $this->db->execStatement(PDO::FETCH_ASSOC);
+    }
+    /**
+     * @param $eid
+     * @return array
+     */
+    public function getReviewOfSystemsChecksByEid($eid)
+    {
+        $this->db->setSQL("SELECT * FROM form_data_review_of_systems_check WHERE eid = '$eid' ORDER BY date DESC");
+        return $this->db->execStatement(PDO::FETCH_ASSOC);
+    }
+    /**
+     * @param $eid
+     * @return array
+     */
+    public function getReviewOfSystemsByEid($eid)
+    {
+        $this->db->setSQL("SELECT * FROM form_data_review_of_systems WHERE eid = '$eid' ORDER BY date DESC");
+        $record = $this->db->fetch();
+        foreach($record as $key => $val){
+            $record[$key] =  ($val == null)? 'null' : $val;
+        }
+        return $record;
+    }
+    /**
+     * @param $eid
+     * @return array
+     */
+    public function getDictationByEid($eid)
+    {
+        $this->db->setSQL("SELECT * FROM form_data_dictation WHERE eid = '$eid' ORDER BY date DESC");
+        return $this->db->execStatement(PDO::FETCH_ASSOC);
+    }
+    /**
+     * @param stdClass $params
+     * @return stdClass
+     */
+    public function updateSoapById(stdClass $params){
+        $data = get_object_vars($params);
+        unset($data['id']);
+        $this->db->setSQL($this->db->sqlBind($data, 'form_data_soap', 'U', "id='".$params->id."'"));
+        $this->db->execLog();
+        return $params;
+    }
+    /**
+     * @param stdClass $params
+     * @return stdClass
+     */
+    public function updateReviewOfSystemsChecksById(stdClass $params)
+    {
+        $data = get_object_vars($params);
+        unset($data['id']);
+        $this->db->setSQL($this->db->sqlBind($data, 'form_data_review_of_systems_check', 'U', "id='".$params->id."'"));
+        $this->db->execLog();
+        return $params;
+    }
+    /**
+     * @param stdClass $params
+     * @return stdClass
+     */
+    public function updateReviewOfSystemsById(stdClass $params)
+    {
+        $data = get_object_vars($params);
+        unset($data['id']);
+        $this->db->setSQL($this->db->sqlBind($data, 'form_data_review_of_systems', 'U', "id='".$params->id."'"));
+        $this->db->execLog();
+        return $params;
+    }
+    /**
+     * @param stdClass $params
+     * @return stdClass
+     */
+    public function updateDictationById(stdClass $params)
+    {
+        $data = get_object_vars($params);
+        unset($data['id']);
+        $this->db->setSQL($this->db->sqlBind($data, 'form_data_dictation', 'U', "id='".$params->id."'"));
+        $this->db->execLog();
+        return $params;
+    }
+    /**
+     * @param $eid
+     * @return array
+     */
+    public function getProgressNoteByEid($eid)
+    {
         $this->db->setSQL("SELECT * FROM form_data_encounter WHERE eid = '$eid'");
         $encounter = $this->db->fetch(PDO::FETCH_ASSOC);
         $encounter['start_date']            = date('F j, Y, g:i a',strtotime($encounter['start_date']));
@@ -313,7 +465,10 @@ class Encounter {
 
         return $encounter;
     }
-
+    /**
+     * @param $date
+     * @return mixed
+     */
     public function parseDate($date)
     {
         return str_replace('T', ' ', $date);
