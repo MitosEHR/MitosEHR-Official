2b4b04ba12c7065bd1d6c97d04e691f3a0b331e7
Emmanuel Carrasquillo <computerfreak_1_5@hotmail.com>
2012-03-25 16:31:27 -0400

    form field sql update

--- app/view/fees/Payments.js
+++ app/view/fees/Payments.js
@@ -12,128 +12,265 @@ Ext.define('App.view.fees.Payments', {
 	extend       : 'App.classes.RenderPanel',
 	id           : 'panelPayments',
 	pageTitle    : 'Payments',
-	pageLayout   : 'border',
 	uses         : ['App.classes.GridPanel'],
-	initComponent: function() {
-		var page = this;
 
-		//******************************************************************
-		// Payments Tab Panel
-		//******************************************************************
-		page.paymentTabPanel = Ext.create('Ext.tab.Panel', {
-			region: 'center',
-			items : [
+    initComponent: function() {
+		var me = this;
+
+		me.panel = Ext.create('Ext.form.Panel', {
+			title: 'Checkout',
+			layout   : 'card',
+            align : 'stretch',
+
+			bbar     : [
 				{
-					title      : 'New Payment',
-					dockedItems: [
-						{
-							xtype: 'toolbar',
-							dock : 'top',
-							items: [
-								Ext.create('Ext.Button', {
-									text   : 'Save',
-									iconCls: 'save',
-									handler: function() {
-
-									}
-								}), '-',
-								Ext.create('Ext.Button', {
-									text   : 'Cancel',
-									iconCls: 'save',
-									handler: function() {
-
-									}
-								}), '-',
-								Ext.create('Ext.Button', {
-									text   : 'Allocate',
-									iconCls: 'save',
-									handler: function() {
-
-									}
-								})
-							]
-						}
-					]
+					id      : 'move-prev',
+					text    : 'Back',
+					handler : function(btn) {
+				    me.navigate(btn.up("panel"), "prev");
+					},
+					disabled: true
 				},
+				'->',
+				//spacer so buttons align to each side
 				{
-					title      : 'Search Payments',
-					dockedItems: [
-						{
-							xtype: 'toolbar',
-							dock : 'top',
-							items: [
-								Ext.create('Ext.Button', {
-									text   : 'Search',
-									iconCls: 'save',
-									handler: function() {
-										page.paymentGrid.show();
-									}
-								}), '-',
-								Ext.create('Ext.Button', {
-									text   : 'Reset',
-									iconCls: 'save',
-									handler: function() {
-										page.paymentGrid.hide();
-									}
-								})
-							]
-						}
-					]
-				},
-				{
-					title      : 'ERA posting',
-					dockedItems: [
-						{
-							xtype: 'toolbar',
-							dock : 'top',
-							items: [
-								Ext.create('Ext.Button', {
-									text   : 'Proccess ERA File',
-									iconCls: 'save',
-									handler: function() {
-
-									}
-								})
-							]
-						}
-					]
+					id      : 'move-next',
+					text    : 'Next',
+					handler : function(btn) {
+						me.navigate(btn.up("panel"), "next");
+					}
 				}
+			],
+			// the panels (or "cards") within the layout
+			items    : [
+
+                {
+                    xtype:'form',
+                    title:'Payment Entry',
+                    defaults:{
+                        bodyStyle:'padding:15px',
+                        bodyBorder:true,
+                        layout:'fit',
+                        labelWidth:110
+                    },
+                    items:[
+                        {
+                            xtype:'container',
+                            layout: {
+                                type   : 'hbox',
+                                align  : 'stretch'
+                            },
+                            height:146,
+                            defaults:{ flex:1 },
+                            items:[
+                                {
+                                    xtype:'fieldset',
+                                    title:'Payment Information',
+                                    layout:'anchor',
+                                    margin:'5 5 5 5',
+                                    defaults: { labelWidth:110 },
+                                    items:[
+                                        {
+                                            fieldLabel: 'Payment Method',
+                                            xtype     : 'mitos.paymentmethodcombo',
+                                            name      : 'paymentmethod'
+                                        },
+                                        {
+                                            xtype:'mitos.currency',
+                                            fieldLabel:'Payment Amount'
+                                        },
+                                        {
+                                            fieldLabel: 'Paying Entity',
+                                            xtype     : 'mitos.payingentitycombo',
+                                            name      : 'paymentmethod'
+                                        },
+                                        {
+                                            fieldLabel: 'Payment Category',
+                                            xtype     : 'mitos.paymentcategorycombo',
+                                            name      : 'paymentmethod'
+                                        }
+                                    ]
+
+                                },
+                                {
+                                    xtype:'fieldset',
+                                    title:'Check Information',
+                                    layout:'anchor',
+                                    margin:'5 5 5 0',
+                                    items:[
+                                        {
+                                            xtype:'datefield',
+                                            fieldLabel:'Check Date'
+                                        },
+                                        {
+                                            xtype:'datefield',
+                                            fieldLabel:'Post To Date'
+                                        },
+                                        {
+                                            xtype:'textfield',
+                                            fieldLabel:'Check Number'
+                                        }
+                                    ]
+                                }
+                            ]
+                        },
+                        {
+                            xtype:'fieldset',
+                            title:'Description',
+                            margin:'0 10 10 10',
+                            height: 112,
+                            items:[
+                                {
+                                    xtype:'fieldcontainer',
+                                    layout: {
+                                        type   : 'hbox',
+                                        align  : 'stretch'
+                                    },
+                                    defaults:{ flex:1 },
+                                    items:[
+                                        {
+                                            xtype:'fieldcontainer',
+                                            items:[
+                                                {
+                                                    xtype:'textfield',
+                                                    fieldLabel:'Payment From'
+                                                },
+                                                {
+                                                    xtype:'textfield',
+                                                    fieldLabel:'Id'
+                                                }
+                                            ]
+                                        },
+                                        {
+                                            xtype:'fieldcontainer',
+                                            items:[
+                                                {
+                                                    xtype:'datefield',
+                                                    fieldLabel:'Deposit Date'
+                                                },
+                                                {
+                                                    xtype:'textareafield',
+                                                    grow:true,
+                                                    name:'note',
+                                                    fieldLabel:'Note',
+                                                    anchor:'100%'
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        },
+                        {
+                            xtype:'form',
+                            title:'Patient CPTs',
+                            margin:'0 10 15 10',
+                            items:[
+                                {
+                                    xtype:'fieldcontainer',
+                                    layout: {
+                                        type   : 'hbox'
+                                    },
+
+                                    defaults:{  flex:1, height:35 },
+
+                                    items:[
+                                        {
+                                            xtype: 'fieldcontainer',
+                                            margin:'0 0 0 45',
+                                            items:[
+                                                {
+                                                    xtype:'textfield',
+                                                    fieldLabel:'Patient Name',
+                                                    labelWidth:110
+                                                }
+                                            ]
+                                        },
+                                        {
+                                            xtype: 'fieldcontainer',
+                                            items:[
+                                                {
+                                                    xtype:'textfield',
+                                                    fieldLabel:'Patient Id',
+                                                    labelWidth:110
+                                                }
+                                            ]
+                                        },
+
+                                        {
+                                            xtype: 'fieldcontainer',
+                                            items:[
+                                                {
+                                                    xtype:'mitos.currency',
+                                                    fieldLabel:'Remaining Amount',
+                                                    labelWidth:110
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                },
+                                {
+                                    xtype  : 'grid',
+                                    title  : 'CPTs',
+                                    columns: [
+                                        {
+                                            header : 'CPT Code',
+                                            flex:1
+                                        },
+                                        {
+                                            header : 'Charge'
+                                        },
+                                        {
+                                            header : 'Copay'
+                                        },
+                                        {
+                                            header : 'Remainder'
+                                        },
+                                        {
+                                            header : 'Allowed'
+                                        },
+                                        {
+                                            header : 'Payment'
+                                        },
+                                        {
+                                            header : 'Adj. Amount'
+                                        },
+                                        {
+                                            header : 'Deductible'
+                                        },
+                                        {
+                                            header : 'Takeback'
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+				}
+
 			]
 		});
-		//******************************************************************
-		// Payment history Grid
-		//******************************************************************
-		page.paymentGrid = Ext.create('App.classes.GridPanel', {
-			title    : 'Payments Search Results',
-			margin   : '3 0 0 0',
-			height   : 300,
-			region   : 'south',
-			hidden   : true,
-			columns  : [
-				{ header: 'id', sortable: false, dataIndex: 'id', hidden: true},
-				{ width: 150, sortable: true, dataIndex: 'date', renderer: Ext.util.Format.dateRenderer('Y-m-d H:i:s') },
-				{ width: 150, sortable: true, dataIndex: 'user' },
-				{ flex: 1, sortable: true, dataIndex: 'body' },
-				{ flex: 1, sortable: true, dataIndex: 'body' },
-				{ flex: 1, sortable: true, dataIndex: 'body' },
-				{ flex: 1, sortable: true, dataIndex: 'body' }
-			],
-			listeners: {
-				itemclick: function() {
 
-				}
-			}
-		});
-		page.pageBody = [ page.paymentTabPanel, page.paymentGrid];
-		page.callParent(arguments);
-	}, // end of initComponent
+
+		me.pageBody = [ me.panel ];
+		me.callParent(arguments);
+	},
+	navigate     : function(panel, direction) {
+
+		var layout = panel.getLayout();
+		layout[direction]();
+		Ext.getCmp('move-prev').setDisabled(!layout.getPrev());
+		Ext.getCmp('move-next').setDisabled(!layout.getNext());
+	},
+
 	/**
 	 * This function is called from MitosAPP.js when
 	 * this panel is selected in the navigation panel.
 	 * place inside this function all the functions you want
 	 * to call every this panel becomes active
 	 */
-	onActive     : function(callback) {
+	onActive: function(callback) {
 		callback(true);
 	}
-}); //ens oNotesPage class
+
+}); //end Payments class
