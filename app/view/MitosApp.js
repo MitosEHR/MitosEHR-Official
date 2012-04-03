f00a4024786f673428f9c03fe367ba2b00e1a097
Ernesto J Rodriguez <vela1606@gmail.com>
2012-04-03 09:29:53 -0400

    billing new layout

--- app/view/MitosApp.js
+++ app/view/MitosApp.js
@@ -105,7 +105,7 @@ Ext.define('App.view.MitosApp', {
             autoLoad:true,
 			listeners: {
 				scope: me,
-				//load : me.navigateToDefault
+				load : me.navigateToDefault
 			}
 		});
 
@@ -702,7 +702,7 @@ Ext.define('App.view.MitosApp', {
 	},
 
 	navigateToDefault: function() {
-		this.navigateTo('panelDashboard');
+		//this.navigateTo('panelDashboard');
 	},
 
 	onNavigationNodeSelected: function(model, selected) {
