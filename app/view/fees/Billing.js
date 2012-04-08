5d9789acb6c6a45785f8fad32bf176cbbc4d75e6
Ernesto J Rodriguez <vela1606@gmail.com>
2012-04-07 23:02:59 -0400

    billing progress

--- app/view/fees/Billing.js
+++ app/view/fees/Billing.js
@@ -7,301 +7,295 @@
 // MitosEHR (Electronic Health Records) 2011
 //******************************************************************************
 Ext.define('App.view.fees.Billing', {
-	extend       : 'App.classes.RenderPanel',
-	id           : 'panelBilling',
-	pageTitle    : 'Billing',
-	uses         : [ 'App.classes.GridPanel' ],
-	pageLayout   : 'card',
-	initComponent: function() {
-		var me = this;
-		me.paymentstatus = 1;
-		me.patient = null;
-		me.pastDue = null;
-		me.dateRange = { start: null, limit: null };
+    extend:'App.classes.RenderPanel',
+    id:'panelBilling',
+    pageTitle:'Billing',
+    uses:[ 'App.classes.GridPanel' ],
+    pageLayout:'card',
+    initComponent:function () {
+        var me = this;
+        me.paymentstatus = 1;
+        me.patient = null;
+        me.pastDue = null;
+        me.dateRange = { start:null, limit:null };
 
-		me.patientListStore = Ext.create('App.store.fees.Billing');
+        me.patientListStore = Ext.create('App.store.fees.Billing');
 
-		me.encountersGrid = Ext.create('Ext.grid.Panel', {
-			store     : me.patientListStore,
-            selModel: Ext.create('Ext.selection.CheckboxModel', {
-                listeners: {
+        me.encountersGrid = Ext.create('Ext.grid.Panel', {
+            store:me.patientListStore,
+            selModel:Ext.create('Ext.selection.CheckboxModel', {
+                listeners:{
                     scope:me,
-                    selectionchange: me.onSelectionChanged
+                    selectionchange:me.onSelectionChanged
                 }
             }),
-			viewConfig: {
-				stripeRows: true
-			},
-			columns   : [
-				{
-					header   : 'Service Date',
-					dataIndex: 'start_date',
-					width    : 200
-				},
-				{
-					header   : 'Patient',
-					dataIndex: 'patientName',
-					width    : 200
-				},
-				{
-					header   : 'Primary Provider',
-					dataIndex: 'primaryProvider',
-					width    : 200
-				},
-				{
-					header   : 'Encounter Provider',
-					dataIndex: 'encounterProvider',
-					flex     : 1
-				},
-				{
-					header   : 'Insurance',
-					dataIndex: 'insurance',
-					width    : 200
-				},
-				{
-					header   : 'Billing Stage',
-					dataIndex: 'billing_stage',
-					renderer : me.stage,
-					width    : 135
-				}
-			],
-			tbar      : [
+            viewConfig:{
+                stripeRows:true
+            },
+            columns:[
+                {
+                    header:'Service Date',
+                    dataIndex:'start_date',
+                    width:200
+                },
+                {
+                    header:'Patient',
+                    dataIndex:'patientName',
+                    width:200
+                },
+                {
+                    header:'Primary Provider',
+                    dataIndex:'primaryProvider',
+                    width:200
+                },
+                {
+                    header:'Encounter Provider',
+                    dataIndex:'encounterProvider',
+                    flex:1
+                },
+                {
+                    header:'Insurance',
+                    dataIndex:'insurance',
+                    width:200
+                },
+                {
+                    header:'Billing Stage',
+                    dataIndex:'billing_stage',
+                    renderer:me.stage,
+                    width:135
+                }
+            ],
+            tbar:[
+                {
+                    xtype:'fieldcontainer',
+                    items:[
+                        {
+                            xtype:'displayfield',
+                            fieldLabel:'Patient Search'
+                        },
+                        {
+                            xtype:'patienlivetsearch',
 
-				{
-					xtype: 'fieldcontainer',
+                            width:235,
+                            margin:'0 5 0 0'
 
-					items: [
-						{
-							xtype: 'displayfield',
-							fieldLabel : 'Patient Search'
-						},
-						{
-							xtype    : 'patienlivetsearch',
+                        }
+                    ]
+                },
+                {
+                    xtype:'fieldcontainer',
+                    items:[
+                        {
+                            xtype:'datefield',
+                            fieldLabel:'From',
+                            labelWidth:35,
+                            action:'datefrom',
+                            width:150
+                        },
+                        {
+                            xtype:'datefield',
+                            fieldLabel:'To',
+                            labelWidth:35,
+                            action:'dateto',
+                            padding:'0 5 0 0',
+                            width:150
+                        }
+                    ]
+                },
+                {
+                    xtype:'fieldcontainer',
+                    items:[
+                        {
+                            xtype:'mitos.providerscombo',
+                            labelWidth:60,
+                            typeAhead:true,
+                            padding:'0 5 0 5',
 
-							width    : 235,
-							margin   : '0 5 0 0'
+                            fieldLabel:'Provider',
+                            defaultValue:'All'
 
-						}
-					]
-				},
-				{
-					xtype: 'fieldcontainer',
-
-					items: [
-						{
-							xtype     : 'datefield',
-							fieldLabel: 'From',
-							labelWidth: 35,
-							action    : 'datefrom',
-							width     : 150
-						},
-						{
-							xtype     : 'datefield',
-							fieldLabel: 'To',
-							labelWidth: 35,
-							action    : 'dateto',
-							padding   : '0 5 0 0',
-							width     : 150
-						}
-					]
-				},
-
-				{
-					xtype: 'fieldcontainer',
-					items: [
-						{
-							xtype       : 'mitos.providerscombo',
-							labelWidth  : 60,
-							typeAhead   : true,
-							padding     : '0 5 0 5',
-
-							fieldLabel  : 'Provider',
-							defaultValue: 'All'
-
-						},
-						{
-							xtype       : 'mitos.insurancepayertypecombo',
-							labelWidth  : 60,
-							padding     : '0 5 0 5',
-							fieldLabel  : 'Insurance',
-							defaultValue: 'All'
+                        },
+                        {
+                            xtype:'mitos.insurancepayertypecombo',
+                            labelWidth:60,
+                            padding:'0 5 0 5',
+                            fieldLabel:'Insurance',
+                            defaultValue:'All'
 
 
-						}
-					]
-				},
+                        }
+                    ]
+                },
+                '->',
+                {
+                    xtype:'tbtext',
+                    text:'Past due:'
+                },
+                {
+                    text:'30+',
+                    enableToggle:true,
+                    action:30,
+                    toggleGroup:'pastduedates',
+                    enableKeyEvents:true,
+                    scale:'large',
+                    listeners:{
+                        scope:me,
+                        click:me.onBtnClicked
+                    }
+                },
+                {
+                    text:'60+',
+                    enableToggle:true,
+                    action:60,
+                    scale:'large',
+                    toggleGroup:'pastduedates',
+                    listeners:{
+                        scope:me,
+                        click:me.onBtnClicked
+                    }
+                },
+                {
+                    text:'120+',
+                    enableToggle:true,
+                    action:120,
+                    scale:'large',
+                    toggleGroup:'pastduedates',
+                    listeners:{
+                        scope:me,
+                        click:me.onBtnClicked
+                    }
+                },
+                {
+                    text:'180+',
+                    enableToggle:true,
+                    action:180,
+                    scale:'large',
+                    toggleGroup:'pastduedates',
+                    listeners:{
+                        scope:me,
+                        click:me.onBtnClicked
+                    }
+                }
+            ],
+            listeners:{
+                scope:me,
+                itemdblclick:me.rowDblClicked
+            }
+        });
 
-				'->',
-				{
-					xtype: 'tbtext',
-					text : 'Past due:'
-				},
-				{
-					text           : '30+',
-					enableToggle   : true,
-					action         : 30,
-					toggleGroup    : 'pastduedates',
-					enableKeyEvents: true,
-					scale   : 'large',
-					listeners      : {
-						scope: me,
-						click: me.onBtnClicked
-					}
+        me.encounterBillingDetails = Ext.create('Ext.panel.Panel', {
+            defaultTitle:'Encounter Billing Details',
+            title:'Encounter Billing Details',
+            layout:'border',
+            items:[
+                me.icdsPanel = Ext.create(''),
 
-				},
-				{
-					text        : '60+',
-					enableToggle: true,
-					action      : 60,
-					scale   : 'large',
-					toggleGroup : 'pastduedates',
-					listeners   : {
-						scope: me,
-						click: me.onBtnClicked
-					}
-				},
-				{
-					text        : '120+',
-					enableToggle: true,
-					action      : 120,
-					scale   : 'large',
-					toggleGroup : 'pastduedates',
-					listeners   : {
-						scope: me,
-						click: me.onBtnClicked
-					}
-				},
-				{
-					text        : '180+',
-					enableToggle: true,
-					action      : 180,
-					scale   : 'large',
-					toggleGroup : 'pastduedates',
-					listeners   : {
-						scope: me,
-						click: me.onBtnClicked
-					}
+                me.cptPanel = Ext.create('App.view.patientfile.encounter.CurrentProceduralTerminology', {
+                    region:'center'
+                }),
+                me.progressNote = Ext.create('App.view.patientfile.ProgressNote', {
+                    title:'Encounter Progress Note',
+                    region:'east',
+                    margin:5,
+                    bodyStyle:'padding:15px',
+                    width:500,
+                    autoScroll:true,
+                    collapsible:true,
+                    animCollapse:true,
+                    collapsed:false
+                })
+            ],
+            buttons:[
+                {
+                    text:'Encounters',
+                    scope:me,
+                    action:'encounters',
+                    tooltip:'Back to Encounter List',
+                    handler:me.onBtnCancel
+                },
+                '->',
+                {
+                    xtype:'tbtext',
+                    action:'page',
+                    text:'( 1 of 1 )'
+                },
+                {
+                    text:'<<<  Back',
+                    scope:me,
+                    action:'back',
+                    tooltip:'Previous Encounter Details',
+                    handler:me.onBtnBack
+                },
+                {
+                    text:'Save',
+                    scope:me,
+                    action:'save',
+                    tooltip:'Save Billing Details'
+                },
 
-				}
-			],
-			listeners : {
-				scope          : me,
-				itemdblclick      : me.rowDblClicked
-			}
+                {
+                    text:'Cancel',
+                    scope:me,
+                    action:'cancel',
+                    tooltip:'Cancel and Go Back to Encounter List',
+                    handler:me.onBtnCancel
+                },
+                {
+                    text:'Next  >>>',
+                    scope:me,
+                    action:'next',
+                    tooltip:'Next Encounter Details',
+                    handler:me.onBtnNext
+                }
+            ]
+        });
 
-		});
-
-		me.encounterBillingDetails = Ext.create('Ext.panel.Panel', {
-			defaultTitle: 'Encounter Billing Details',
-			title       : 'Encounter Billing Details',
-			layout      : 'border',
-			items       : [
-				me.cptPanel = Ext.create('App.view.patientfile.encounter.CurrentProceduralTerminology', {
-					region: 'center'
-				}),
-				me.progressNote = Ext.create('App.view.patientfile.ProgressNote', {
-					title       : 'Encounter Progress Note',
-					region      : 'east',
-					margin      : 5,
-					bodyStyle   : 'padding:15px',
-					width       : 500,
-					autoScroll  : true,
-					collapsible : true,
-					animCollapse: true,
-					collapsed   : false
-				})
-			],
-			buttons     : [
-				{
-					text   : 'Encounters',
-					scope  : me,
-					action : 'encounters',
-					tooltip: 'Back to Encounter List',
-					handler: me.onBtnCancel
-				},
-				'->',
-				{
-					xtype : 'tbtext',
-					action: 'page',
-					text  : '( 1 of 1 )'
-				},
-				{
-					text   : '<<<  Back',
-					scope  : me,
-					action : 'back',
-					tooltip: 'Previous Encounter Details',
-					handler: me.onBtnBack
-				},
-				{
-					text   : 'Save',
-					scope  : me,
-					action : 'save',
-					tooltip: 'Save Billing Details'
-				},
-
-				{
-					text   : 'Cancel',
-					scope  : me,
-					action : 'cancel',
-					tooltip: 'Cancel and Go Back to Encounter List',
-					handler: me.onBtnCancel
-				},
-				{
-					text   : 'Next  >>>',
-					scope  : me,
-					action : 'next',
-					tooltip: 'Next Encounter Details',
-					handler: me.onBtnNext
-				}
-			]
-		});
-
-		me.pageBody = [ me.encountersGrid, me.encounterBillingDetails ];
-		me.callParent(arguments);
-	}, // end of initComponent
+        me.pageBody = [ me.encountersGrid, me.encounterBillingDetails ];
+        me.callParent(arguments);
+    }, // end of initComponent
 
 
-    stage:function(val){
-        if(val == '1') {
+    stage:function (val) {
+        if (val == '1') {
             return '<img src="ui_icons/stage1.png" />';
-        } else if(val == '2') {
+        } else if (val == '2') {
             return '<img src="ui_icons/stage2.png" />';
-        } else if(val == '3') {
+        } else if (val == '3') {
             return '<img src="ui_icons/stage3.png" />';
-        } else if(val == '4') {
+        } else if (val == '4') {
             return '<img src="ui_icons/stage4.png" />';
         }
         return val;
     },
 
-	onBtnClicked: function(btn) {
-		var datefrom = this.query('datefield[action="datefrom"]'),
-			dateto = this.query('datefield[action="dateto"]');
-		if(btn.pressed) {
-			datefrom[0].reset();
-			dateto[0].reset();
-			this.pastDue = btn.action;
-		} else {
-			this.pastDue = 0;
-		}
-		this.reloadGrid();
+    onBtnClicked:function (btn) {
+        var datefrom = this.query('datefield[action="datefrom"]'),
+            dateto = this.query('datefield[action="dateto"]');
+        if (btn.pressed) {
+            datefrom[0].reset();
+            dateto[0].reset();
+            this.pastDue = btn.action;
+        } else {
+            this.pastDue = 0;
+        }
+        this.reloadGrid();
 
-	},
+    },
 
-	rowDblClicked: function() {
-		this.goToEncounterBillingDetail();
-	},
+    rowDblClicked:function () {
+        this.goToEncounterBillingDetail();
+    },
 
-	goToEncounterBillingDetail: function() {
-		this.getPageBody().getLayout().setActiveItem(1);
-	},
+    goToEncounterBillingDetail:function () {
+        this.getPageBody().getLayout().setActiveItem(1);
+    },
 
-	goToEncounterList: function() {
-		this.getPageBody().getLayout().setActiveItem(0);
-	},
+    goToEncounterList:function () {
+        this.getPageBody().getLayout().setActiveItem(0);
+    },
 
-	onSelectionChanged: function(sm, model) {
-        if(model[0]){
+    onSelectionChanged:function (sm, model) {
+        if (model[0]) {
             var me = this,
                 title = me.encounterBillingDetails.defaultTitle,
                 backbtn = me.encounterBillingDetails.query('button[action="back"]'),
@@ -314,7 +308,7 @@ Ext.define('App.view.fees.Billing', {
             me.encounterBillingDetails.setTitle(title + ' ( ' + model[0].data.patientName + ' )');
 
             me.cptPanel.pid = model[0].data.pid;
-            me.cptPanel.encounterCptStoreLoad(model[0].data.eid, function(){
+            me.cptPanel.encounterCptStoreLoad(model[0].data.eid, function () {
                 me.cptPanel.setDefaultQRCptCodes();
             });
 
@@ -322,59 +316,59 @@ Ext.define('App.view.fees.Billing', {
             nextBtn[0].setDisabled(rowIndex == sm.store.data.length - 1);
             backbtn[0].setDisabled(rowIndex == 0);
         }
-	},
+    },
 
-	onBtnCancel: function() {
-		this.getPageBody().getLayout().setActiveItem(0);
-	},
+    onBtnCancel:function () {
+        this.getPageBody().getLayout().setActiveItem(0);
+    },
 
-	onBtnBack: function() {
-		var sm = this.encountersGrid.getSelectionModel(),
-			currRowIndex = sm.getLastSelected().index,
-			prevRowindex = currRowIndex - 1;
-		sm.select(prevRowindex);
-	},
+    onBtnBack:function () {
+        var sm = this.encountersGrid.getSelectionModel(),
+            currRowIndex = sm.getLastSelected().index,
+            prevRowindex = currRowIndex - 1;
+        sm.select(prevRowindex);
+    },
 
-	onBtnNext: function() {
-		var sm = this.encountersGrid.getSelectionModel(),
-			currRowIndex = sm.getLastSelected().index,
-			nextRowindex = currRowIndex + 1;
-		sm.select(nextRowindex);
-	},
+    onBtnNext:function () {
+        var sm = this.encountersGrid.getSelectionModel(),
+            currRowIndex = sm.getLastSelected().index,
+            nextRowindex = currRowIndex + 1;
+        sm.select(nextRowindex);
+    },
 
-	onBtnSave: function() {
-		this.goToEncounterList();
-	},
+    onBtnSave:function () {
+        this.goToEncounterList();
+    },
 
-	reloadGrid: function() {
-		this.patientListStore.load({
-			params: {
-				query: {
-					patient  : this.patient,
-					pastDue  : this.pastDue,
-					dateRange: this.dateRange
-				}
-			}
-		});
-	},
+    reloadGrid:function () {
+        this.patientListStore.load({
+            params:{
+                query:{
+                    patient:this.patient,
+                    pastDue:this.pastDue,
+                    dateRange:this.dateRange
+                }
+            }
+        });
+    },
 
-	updateProgressNote: function(eid) {
-		var me = this;
-		Encounter.getProgressNoteByEid(eid, function(provider, response) {
-			var data = response.result;
-			me.progressNote.tpl.overwrite(me.progressNote.body, data);
-		});
-	},
+    updateProgressNote:function (eid) {
+        var me = this;
+        Encounter.getProgressNoteByEid(eid, function (provider, response) {
+            var data = response.result;
+            me.progressNote.tpl.overwrite(me.progressNote.body, data);
+        });
+    },
 
 
-	/**
-	 * This function is called from MitosAPP.js when
-	 * this panel is selected in the navigation panel.
-	 * place inside this function all the functions you want
-	 * to call every this panel becomes active
-	 */
-	onActive: function(callback) {
-		this.reloadGrid();
-		callback(true);
-	}
+    /**
+     * This function is called from MitosAPP.js when
+     * this panel is selected in the navigation panel.
+     * place inside this function all the functions you want
+     * to call every this panel becomes active
+     */
+    onActive:function (callback) {
+        this.reloadGrid();
+        callback(true);
+    }
 }); //ens oNotesPage class
