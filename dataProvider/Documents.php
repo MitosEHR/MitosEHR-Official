b54ced5981e556e09b89bd21fe102bfcedfd24c5
Ernesto J Rodriguez <vela1606@gmail.com>
2012-05-13 12:04:10 -0400

    DomPDF for the win

--- dataProvider/Documents.php
+++ dataProvider/Documents.php
@@ -41,10 +41,6 @@ class Documents
 	 * @var Facilities
 	 */
 	private $facility;
-	/**
-	 * @var PDF
-	 */
-	private $pdf;
 
 	function __construct()
 	{
@@ -53,98 +49,12 @@ class Documents
 		$this->patient  = new Patient();
 		$this->services = new Services();
 		$this->facility = new Facilities();
-		$this->pdf      = new PDF();
 		return;
 	}
 
-    private $patient_name='Julio Alisea Acosta';
-    private $edad='23';
-    private $direccion='Villas de Quintana APT 1324, Hato Rey, PR 00987';
-    private $fecha='12/01/2012';
-    private $doctor='Dr.Rodriguez';
-    private $work = 'Yes';
-    private $symptons = 'Headache';
-    private $comment_number = '2';
-    private $paragraph ='Has been under care of ';
-    private $from ='12/01/2012';
-    private $to ='12/01/2012';
-    private $return ='12/01/2012';
-    private $num ='787-787-8787';
-    private $first='___';
-    private $second='___';
-    private $third='___';
-    private $fourth='___';
-
 	public function createSuperBillDoc(stdClass $params)
 	{
-        //Header of the pdf document
-        $this->pdf->AddPage();
-
-// patient          age
-// adress    zipcode  fecha
-        //First Blue Row contains Facility information
-		//$this->pdf->AddPage();
-        $this->pdf->SetFont('Arial', 'B', 16);
-        $this->pdf->Cell(0, 6, 'CERTIFICATE TO RETURN TO WORK', 0, 1, 'C');
-        $this->pdf->Ln(8);
-		$this->pdf->SetFont('Arial', 'B', 16);
-        $this->pdf->Cell(0, 6, 'Patient Information: ', 0, 1, 'L');
-        $this->pdf->Ln(1);
-		$this->pdf->SetFont('Arial', '', 14);
-		$this->pdf->Cell(0, 6, 'Name: '.$this->patient_name.'    Age: '.$this->edad.'   Date: '.$this->fecha, 0, 1, 'L');
-		$this->pdf->Cell(0, 6, 'Address: '.$this->direccion, 0, 1, 'L');
-
-        $this->pdf->Ln(4);
-        $this->pdf->Cell(0,6, $this->paragraph.$this->doctor,0,1,'L');
-        $this->pdf->Cell(0,6, 'From: '.$this->from.' To:'.$this->to,0,1,'L');
-
-        $this->pdf->Ln(4);
-        $this->pdf->Cell(0,6,'Return to work on: '. $this->return,0,1,'L');
-
-        $this->pdf->Ln(4);
-
-        $this->pdf->Cell(0,6, 'Nature of illness or injury: '.$this->symptons,0,1,'L');
-
-		//$this->pdf->MultiCell(0, 5, $this->patient->getPatientNameById($params->pid));
-        //$this->pdf->MultiCell(0, 5, $this->patient->getPatientAdditionalInfoById($params->pid));
-        $this->pdf->Ln(5);
-        $this->pdf->SetFont('Arial', 'B', 16);
-        $this->pdf->Cell(0,6, 'Physical activities: ',0,1,'L');
-        $this->pdf->Ln(5);
-
-        if($this->comment_number == 1){
-            $this->first ='_X_';
-        }else if($this->comment_number == 2){
-
-            $this->second ='_X_';
-        }else if($this->comment_number == 3){
-
-            $this->third ='_X_';
-        }else if($this->comment_number == 4){
-
-            $this->fourth ='_X_';
-        }
-
-        $this->pdf->SetFont('Arial', '', 14);
-        $this->pdf->Cell(0, 6,$this->first.' Patient can not drive.', 0, 1, 'L');
-        $this->pdf->Cell(0, 6,$this->second.' Patient can not lift heavy objects.', 0, 1, 'L');
-        $this->pdf->Cell(0, 6,$this->third.' Patient can not use stairs.', 0, 1, 'L');
-        $this->pdf->Cell(0, 6,$this->fourth.' Other.', 0, 1, 'L');
-        $this->pdf->Ln(5);
-
-        $this->pdf->SetFont('Arial', '', 16);
-        $this->pdf->Cell(0,6, 'For more information, please call ',0,1,'L');
-        $this->pdf->Cell(0,6, $this->num,0,1,'L');
-        $this->pdf->Ln(15);
-
-        $this->pdf->SetFont('Arial', '', 14);
-        $this->pdf->Cell(0, 6,' Physician:______________________________', 0, 1, 'L');
-
-
-
-		//$this->pdf->Output('hello.pdf', 'F');
-        $this->pdf->Output();
-
+        return;
 	}
 
 	/**
@@ -175,14 +85,6 @@ class Documents
 	}
 
 }
-
-$params             = new stdClass();
-$params->pid        = 1;
-$params->fid        = 6;
-$params->start_date = '2012-01-01';
-$params->stop_date  = '2012-03-01';
-$docs               = new Documents();
-$docs->createSuperBillDoc($params);
 
 
 
